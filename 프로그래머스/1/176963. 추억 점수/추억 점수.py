c81e06dee403453bb1cfb2970d6e7779fa50da37
hyowonsong <122877101+hyowonsong@users.noreply.github.com>
2024-03-01 18:53:03 +0900

    [level 1] Title: 추억 점수, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.py
@@ -0,0 +1,10 @@
+def solution(name, yearning, photo):
+    answer = []
+    
+    for match in photo:
+        score = 0
+        for n in match:
+            if n in name:
+                score += yearning[name.index(n)]
+        answer.append(score)
+    return answer
