421e6bac451112ccab642b8da281ee6ef3bf0034
hyowonsong <122877101+hyowonsong@users.noreply.github.com>
2024-03-01 17:58:46 +0900

    [level 2] Title: 구명보트, Time: 8.78 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -6,11 +6,12 @@ def solution(people, limit):
     start = 0
     end = len(people) -1
     
-    while(start <= end):
-        if(people[start] + people[end] <= limit):
+    while(end>=start):
+        if (people[start] + people[end] <= limit):
             start +=1
             end -=1
+            
         else:
             end -=1
-        answer += 1
+        answer +=1
     return answer
