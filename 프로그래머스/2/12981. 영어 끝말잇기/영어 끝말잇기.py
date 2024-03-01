26187e1fde8b43c1ce91714f50530783d7a81cec
hyowonsong <122877101+hyowonsong@users.noreply.github.com>
2024-03-01 17:48:52 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.09 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,16 +1,18 @@
 def solution(n, words):
     used_words = []
-    used_words.append(words[0])
-    last_words = words[0][-1]
-    number, order = 0, 0              # 여기 0, 0 이어야 된다.
+    number, order = 0,0
     
-    for i in range(1,len(words)):      # 맨 처음에 0은 위에서 지정해줬으니 1부터
-        if words[i] not in used_words and words[i][0] == last_words:
+    used_words.append(words[0])
+    last_word = words[0][-1]
+    
+    for i in range(1, len(words)):
+        if words[i] not in used_words and words[i][0] == last_word:
             used_words.append(words[i])
-            last_words = words[i][-1]
+            last_word = words[i][-1]
+            
         else:
-            number = (i%n) + 1
+            number = (i%n) +1
             order = (i//n) +1
             break
             
-    return [number, order]
+    return [number,order]
