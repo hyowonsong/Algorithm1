1fc2c6d24c4e0dece8b2658ca6de2e82b4f78ade
hyowonsong <122877101+hyowonsong@users.noreply.github.com>
2024-03-01 17:16:45 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,17 +1,15 @@
 def solution(n, arr1, arr2):
     answer = []
-    
     for i in range(n):
-        tmp = bin(arr1[i] | arr2[i])
-        # tmp결과 ex) '0b1101'
-        
+        tmp = bin(arr1[i]| arr2[i])
         tmp = tmp[2:].zfill(n)
-        # tmp결과 ex) '01101' -> 0b로 나오기 때문에 앞에 2개 제거하고 zfill 사용
-        # zfil(n)을 하면 n만큼 부족한 부분을 앞에 0으로 채워준다.
-        
         tmp = tmp.replace('1','#').replace('0',' ')
-        # 1을 #으로, 0을 공백으로 치환
-        
         answer.append(tmp)
-    
+        
     return answer
+
+n =5
+arr1 = [9, 20, 28, 18, 11]
+arr2 = [30, 1, 21, 17, 28]
+
+print(solution(n, arr1, arr2))
