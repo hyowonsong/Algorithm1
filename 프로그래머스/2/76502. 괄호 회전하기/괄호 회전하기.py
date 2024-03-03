1d24793eaf022145828de8b03527d6e20fe4b862
hyowonsong <122877101+hyowonsong@users.noreply.github.com>
2024-03-03 15:45:12 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -0,0 +1,28 @@
+def bracket(s):
+        stack = []
+        for i in s:
+            if len(stack) == 0: 
+                stack.append(i)
+            else:
+                if i == ")" and stack[-1] == "(":   
+                    stack.pop()
+                elif i == "]" and stack[-1] == "[":   
+                    stack.pop()
+                elif i == "}" and stack[-1] == "{":   
+                    stack.pop()
+                else: 
+                    stack.append(i)
+        if len(stack) == 0:
+            return 1
+        else:
+            return 0
+
+        
+def solution(s):
+    answer = 0
+    for i in range(len(s)):
+        # 위의 bracket(s) 의 정의가 맞으면 answer +=1이다.
+        if bracket(s):  
+            answer +=1
+        s = s[1:] + s[:1]      # 문자열 's'를 왼쪽으로 한 칸 회전하는 연산
+    return answer
