42aed7239e1d730997184b9950f529bb18986e2c
hyowonsong <122877101+hyowonsong@users.noreply.github.com>
2024-04-17 20:42:22 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,15 +1,10 @@
 def solution(n, arr1, arr2):
     answer = []
+    # arr1과 arr2 겹쳐야하고 전부 2진수로 바꿔줘야 한다.
     for i in range(n):
-        tmp = bin(arr1[i]| arr2[i])
+        tmp = bin(arr1[i]|arr2[i])
         tmp = tmp[2:].zfill(n)
-        tmp = tmp.replace('1','#').replace('0',' ')
-        answer.append(tmp)
-        
+        tmp = tmp.replace('1', '#').replace('0',' ')
+        answer.append(tmp)    
     return answer
-
-n =5
-arr1 = [9, 20, 28, 18, 11]
-arr2 = [30, 1, 21, 17, 28]
-
-print(solution(n, arr1, arr2))
+    
