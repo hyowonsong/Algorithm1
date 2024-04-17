7cc7b8c4fb14cb77324b5c3550632be8cb916345
hyowonsong <122877101+hyowonsong@users.noreply.github.com>
2024-04-17 20:54:39 +0900

    [level 1] Title: K번째수, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -2,9 +2,7 @@ def solution(array, commands):
     answer = []
     
     for i in commands:
-        arr = array[i[0]-1:i[1]]        
-        arr.sort()
-        answer.append(arr[i[2]-1])
-    
-    
+        part = array[i[0]-1:i[1]]
+        part.sort()
+        answer.append(part[i[2]-1])
     return answer
