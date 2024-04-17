68d080e6e2261be182ce168a221838c1b12e1993
hyowonsong <122877101+hyowonsong@users.noreply.github.com>
2024-04-17 12:47:20 +0900

    [level 2] Title: 숫자의 표현, Time: 4.95 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -3,7 +3,7 @@ def solution(n):
     sum = 0
     
     for i in range(1,n+1):
-        sum = 0
+        sum = 0                    # 초기화
         for j in range(i, n+1):    # j는 i부터 더해나가는 역할
             sum += j
             
