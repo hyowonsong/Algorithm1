e8a9cc642ba1b1b822c40193598f28a8908ca37c
hyowonsong <122877101+hyowonsong@users.noreply.github.com>
2024-04-18 15:44:36 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.40 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -1,9 +1,19 @@
+# 멀리 뛰기
+# 한번에 1칸 또는 2칸(dp 문제 같은데)
+
+# n=1 ->1
+# n=2 ->2
+# n=3 ->3
+# n=4 ->5
+# n=5 ->8
+
 def solution(n):
     if n<3:
         return n
-    d = [0]*(n+1)
-    d[1] = 1
-    d[2] = 2
-    for i in range(3,n+1):
-        d[i] = d[i-1] + d[i-2]
-    return d[n] % 1234567
+    else:
+        dp = [0]*(n+1)
+        dp[1] = 1
+        dp[2] = 2
+        for i in range(3,n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n] % 1234567
