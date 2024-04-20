a68da55a5eb3d04271612c94abe225c296c7cd2a
hyowonsong <122877101+hyowonsong@users.noreply.github.com>
2024-04-20 10:57:40 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,16 +1,14 @@
 def solution(progresses, speeds):
-    # 결과를 담을 리스트
     answer = []
-    # 작업 리스트가 빌 때까지 반복
     while progresses:
-        # 몇 개의 기능이 배포되는지 저장 
         cnt = 0
         # 작업 리스트의 진도를 증가시킴
         for i in range(len(progresses)):
             progresses[i] += speeds[i]
         
         # 맨 앞의 작업의 진도가 100 이상인 동안 반복
-        while progresses and progresses[0] >= 100:
+        # while문을 써줘서 이거 계속 될동안 반복
+        while progresses and progresses[0] >= 100:      
             cnt += 1
             # 해당 작업과 작업 속도를 리스트에서 제거
             progresses.pop(0)
