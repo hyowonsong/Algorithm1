75846ff0c810a50b7e634b98d746c97120cb130f
hyowonsong <122877101+hyowonsong@users.noreply.github.com>
2024-04-21 14:37:23 +0900

    [level 2] Title: 피로도, Time: 0.07 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,21 +1,22 @@
-# 피로도
 
-answer = 0  # 최대 던전 클리어 횟수를 저장하는 전역 변수
-
-def dfs(k, cnt, dungeons, visited):
-    global answer 
-    if cnt > answer:
-        answer = cnt  # 현재까지의 클리어 횟수가 최대 클리어 횟수보다 크면 갱신
-
-    for i in range(len(dungeons)):
-        # 방문하지 않았고, 현재 피로도로 해당 던전을 클리어할 수 있다면
-        if not visited[i] and k >= dungeons[i][0]:
-            visited[i] = True  # 해당 던전을 방문 표시
-            dfs(k - dungeons[i][1], cnt + 1, dungeons, visited)  # 재귀적으로 다음 상태 탐색
-            visited[i] = False  # 해당 던전 방문 표시를 되돌림
+from itertools import permutations
 
 def solution(k, dungeons):
-    global answer
-    visited = [False] * len(dungeons)
-    dfs(k, 0, dungeons, visited)  # 초기 피로도, 클리어 횟수 0, 던전 정보, 방문 여부를 초기값으로 DFS 호출
-    return answer  # 최대 던전 클리어 횟수 반환
+    max_dungeons = 0
+    
+    # 던전들의 순서를 모두 조합하여 확인
+    for perm in permutations(dungeons):
+        current_fatigue = k
+        count = 0
+        
+        for dungeon in perm:
+            min_fatigue, consume_fatigue = dungeon
+            # 현재 피로도로 해당 던전을 탐험할 수 있는지 확인
+            if current_fatigue >= min_fatigue:
+                count += 1
+                current_fatigue -= consume_fatigue
+                
+        # 최대 던전 수 업데이트
+        max_dungeons = max(max_dungeons, count)
+    
+    return max_dungeons
