b614016fa810429ea96a995ad3e8f9ed46e1e858
hyowonsong <122877101+hyowonsong@users.noreply.github.com>
2024-04-21 09:53:15 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,15 +1,15 @@
-# 의상(해시맵을 사용한다는 것을 체크!)
+# 의상
 
 def solution(clothes):
-    #  옷을 종류별로 구분하기
+    answer = 0 
     hash_map = {}
     for clothe, type in clothes:
+        # 해시 맵에서 type에 아무것도 없으면 0해주고 +1
         hash_map[type] = hash_map.get(type, 0) + 1
-        
-    #  입지 않는 경우를 추가하여 모든 조합 계산하기
-    answer = 1
-    for type in hash_map:   
-        answer *= (hash_map[type] + 1)
+
     
-    #  아무종류의 옷도 입지 않는 경우 제외하기
+    answer = 1
+    # 여기서 입지 않는 경우를 추가해서 모든 조합 계산했어야
+    for type in hash_map:
+        answer *= hash_map[type] + 1
     return answer - 1
