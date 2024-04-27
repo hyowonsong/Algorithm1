3d0638d1ced1c10901d41e1f38401ed43f5bf960
hyowonsong <122877101+hyowonsong@users.noreply.github.com>
2024-04-27 13:55:51 +0900

    [level 1] Title: 덧칠하기, Time: 3.18 ms, Memory: 12 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
@@ -0,0 +1,9 @@
+def solution(n, m, section):
+    answer = 1 # 칠하는 횟수
+    paint = section[0] # 덧칠 시작점
+    for i in range(1, len(section)):
+        if section[i] - paint >= m:
+            answer += 1
+            paint = section[i]       # i로 초기해줘야 덧칠 시작점이 바뀌지
+            
+    return answer
