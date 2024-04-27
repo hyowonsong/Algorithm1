d3078b5b56045b239424c4fa9b0bb37f064475c4
hyowonsong <122877101+hyowonsong@users.noreply.github.com>
2024-04-27 13:13:59 +0900

    [level 2] Title: 전화번호 목록, Time: 83.36 ms, Memory: 28 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,18 +1,9 @@
-# 해시맵으로 구현 -> 공간을 배열보다 많이 배정해서 찾는 시간을 줄이자
-
-def solution(phone_book): 
-    # 1. Hash map 생성
-    hash_map = {}
-    for nums in phone_book:
-        hash_map[nums] = 1
+def solution(phone_book):
+    phone_book.sort()
     
-    # 2. 접두어가 Hash map에 존재하는지 찾기
-    for nums in phone_book:
-        arr = ""
-        for num in nums:
-            arr+=num
-            
-            # 3. 본인 자체인 경우는 제외
-            if arr in hash_map and arr != nums:
-                return False
+    # 정렬된 전화번호부를 순회하면서 인접한 두 번호끼리 접두어 관계인지 확인
+    for i in range(len(phone_book) - 1):
+        if phone_book[i+1].startswith(phone_book[i]):
+            return False
+    
     return True
