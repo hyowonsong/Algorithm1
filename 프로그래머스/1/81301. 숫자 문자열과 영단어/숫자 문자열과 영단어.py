169bad4d56cce95486b241764629ce5c6110a93b
hyowonsong <122877101+hyowonsong@users.noreply.github.com>
2024-07-05 19:14:48 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,15 +1,8 @@
 def solution(s):
-    dictionary ={'zero':0,
-           'one' : 1,
-           'two' : 2,
-           'three': 3,
-           'four' :4,
-           'five' : 5,
-           'six':6,
-           'seven':7,
-           'eight':8,
-           'nine':9,
-           }
-    for key,value in dictionary.items():
+    dict = {'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4,
+                'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}
+
+    for key,value in dict.items():         # dict.items()는 딕셔너리의 모든 키,값 쌍을 반환
         s = s.replace(key,str(value))
     return int(s)
+    
