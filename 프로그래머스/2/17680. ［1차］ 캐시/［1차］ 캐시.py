eca6cac764918bb3fda429ff73178b3c6f58b826
hyowonsong <122877101+hyowonsong@users.noreply.github.com>
2024-07-06 19:59:57 +0900

    [level 2] Title: [1차] 캐시, Time: 0.81 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -1,21 +1,21 @@
-# 캐시
+# 캐시(캐시 크기에 따른 실행시간 측정 프로그램)
+
 from collections import deque
 
 def solution(cacheSize, cities):
     answer = 0
-    # 이게 캐시(현재 cache 라고 정의해 둠)
-    cache = deque(maxlen = cacheSize)
+
+    cache = deque(maxlen=cacheSize)
 
     for city in cities:
         city_lower = city.lower()
-        # 캐시히트이면
+        # 캐시히트이면 최신으로 유지해야하기 때문에 remove 필요
         if city_lower in cache:
             answer += 1
             cache.remove(city_lower)
             cache.append(city_lower)
-
-        # 캐시미스이면
+        # 캐시미스이면 가장 오래된 것이 자동으로 삭제되기 때문에 remove 필요없다. 
         else:
             cache.append(city_lower)
-            answer +=5
+            answer += 5
     return answer
