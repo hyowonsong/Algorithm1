b0eb462a43b0693ed2f4c233263231f8e91fd79f
hyowonsong <122877101+hyowonsong@users.noreply.github.com>
2024-07-06 20:27:29 +0900

    [level 2] Title: 튜플, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.py
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -1,13 +1,17 @@
-# 튜플
-
 def solution(s):
-    li=[]
-    for i in s.split("},"):
-        li.append(i.replace("{","").replace("}","").split(","))
-    li.sort(key=len)
-    answer=[]
-    for i in li:
-        for j in i:
-            if j not in answer:
-                answer.append(j)
-    return list(map(int,answer))
+    # 문자열에서 집합을 추출
+    sets = s[2:-2].split('},{')
+    
+    # 각 집합을 리스트로 변환하고, 길이 순으로 정렬
+    sets = [set(map(int, x.split(','))) for x in sets]
+    sets.sort(key=len)
+    
+    answer = []
+    for subset in sets:
+        # 집합에서 아직 추가되지 않은 원소를 찾아서 추가
+        for num in subset:
+            if num not in answer:
+                answer.append(num)
+                break
+    
+    return answer
