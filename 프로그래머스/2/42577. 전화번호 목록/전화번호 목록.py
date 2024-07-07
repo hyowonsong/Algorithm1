fb743b3c42a3d88834492a73aa52745a0ff272ae
hyowonsong <122877101+hyowonsong@users.noreply.github.com>
2024-07-07 17:26:56 +0900

    [level 2] Title: 전화번호 목록, Time: 190.62 ms, Memory: 34.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,20 +1,20 @@
+# 전화번호 목록
+
 def solution(phone_book):
     # 해시 맵 초기화
     hash_map = {}
-    
+
     # 각 전화번호의 해시 값을 계산하여 해시 맵에 저장
     for phone_number in phone_book:
         hash_map[phone_number] = True
-    
+
     # 각 전화번호의 접두어가 해시 맵에 있는지 확인
     for phone_number in phone_book:
         prefix = ""
         for digit in phone_number:
-            prefix += digit
+            prefix += digit      # prefix라는 변수에 digit을 하나씩 추가하여 접두어 형성
             # 접두어가 해시 맵에 있다면 해당 전화번호는 다른 번호의 접두어
             if prefix != phone_number and prefix in hash_map:
                 return False
     
     return True
-
-solution(["119", "97674223", "1195524421"])
