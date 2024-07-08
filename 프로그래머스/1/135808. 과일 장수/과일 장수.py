dffdab2871845644de6fcf4764739f3b203a7db8
hyowonsong <122877101+hyowonsong@users.noreply.github.com>
2024-07-08 21:35:21 +0900

    [level 1] Title: 과일 장수, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -6,6 +6,7 @@ def solution(k, m, score):
 
      # m개씩 나누는거 생각해야지 (for 문 사용)
     for i in range(0, len(score), m):
-        if len(score[i:i+m]) == m:
+        # score 길이: len(score) i:i+m 까지가 m 이면 
+        if len(score[i:i+m]) == m: 
             answer += min(score[i:i+m])*m
     return answer
