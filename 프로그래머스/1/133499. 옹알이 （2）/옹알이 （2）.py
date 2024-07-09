dcc55909e8c3a038db6242e70534c39d62966b8f
hyowonsong <122877101+hyowonsong@users.noreply.github.com>
2024-07-09 21:40:21 +0900

    [level 1] Title: 옹알이 (2), Time: 0.05 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
@@ -1,15 +1,18 @@
-# 옹알이
+# 옹알이(2)
 
 def solution(babbling):
-    answer = 0
-    speak = ['aya', 'ye', 'woo', 'ma']
+    answer = 0  
+    speak = ['aya', 'ye', 'woo', 'ma']  
     
     for bab in babbling:
+        # 각 단어에 대해 처리
         for j in speak:
+            # 연속된 같은 발음이 없을 때만 발음을 빈 문자열로 대체
             if j*2 not in bab:
                 bab = bab.replace(j,' ')
                 
+        # 공백만 남았으면 조건에 맞는 단어로 간주
         if bab.isspace():
-            answer +=1
+            answer += 1
             
-    return answer
+    return answer  # 조건을 만족하는 단어의 개수를 반환
