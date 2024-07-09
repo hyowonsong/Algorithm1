81c75801aeb692e0848dcd3a51ec36d57936f04a
hyowonsong <122877101+hyowonsong@users.noreply.github.com>
2024-07-09 22:54:27 +0900

    [level 2] Title: 주식가격, Time: 107.42 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,14 +1,20 @@
+# 주식가격
+
 def solution(prices):
-    answer = [0] * len(prices) # 결과를 저장할 배열 초기화
-    
-    for i in range(len(prices)):
-        for j in range(i+1, len(prices)):
-            # 현재 가격보다 떨어지는 경우가 나타날 때까지 시간을 증가시킴
+    n = len(prices)
+    # 배열 초기화
+    answer = [0]*n
+
+    for i in range(n):
+        for j in range(i+1, n):
+            # i가 j보다 크면 가격이 떨어진 것이므로 j-i 만큼의 시간 계산
             if prices[i] > prices[j]:
-                answer[i] = j - i
+                answer[i] = j-i
                 break
             # 가격이 떨어지지 않은 경우
             elif j == len(prices) - 1:
-                answer[i] = j - i
-                
+                answer[i] = j-i
+
     return answer
+
+
