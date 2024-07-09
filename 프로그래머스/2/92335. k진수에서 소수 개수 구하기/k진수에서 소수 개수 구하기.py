9548910913b9a564e188f2cc1c69652cb66cd952
hyowonsong <122877101+hyowonsong@users.noreply.github.com>
2024-07-09 21:24:33 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
@@ -12,14 +12,18 @@ def solution(n, k):
     
     # n을 k진수로 변환
     while n > 0:
+        # 현재 변환된 숫자의 앞에 추가하여 k진수 문자열을 만듭니다.
         converted = str(n % k) + converted
+        # n을 k로 나눈 몫으로 갱신합니다. 이렇게 하면 다음 자릿값을 계산
         n //= k
     
     # 변환된 수에서 소수 찾기
+    # k진수로 변환된 문자열을 0을 기준으로 분리
     tokens = converted.split('0')
     for token in tokens:
         if token == '':
             continue
+        # 각 분리된 문자열이 소수인지 확인하고, 소수이면 answer를 1 증가
         if is_prime(int(token)):
             answer += 1
     
