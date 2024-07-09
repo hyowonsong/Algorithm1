dddff646e227a707cb11ceb6394ac3401c1363be
hyowonsong <122877101+hyowonsong@users.noreply.github.com>
2024-07-09 21:57:45 +0900

    [level 2] Title: 모음 사전, Time: 1.57 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,17 +1,17 @@
-# 모음사전 
-
 from itertools import product
 
 def solution(word):
-    answer = 0
+    answer = []  # 단어들을 저장할 리스트
     
-    result = list()
+    li = ['A', 'E', 'I', 'O', 'U']  # 사용할 알파벳 모음 리스트
+    
+    # 길이가 1부터 5까지의 모든 조합 생성하여 answer에 저장
     for i in range(1, 6):
-        li = list(product(["A", "E", "I", "O", "U"], repeat=i))
-        for j in li:
-            result.append(''.join(k for k in j))
-        
-    result.sort()
-    answer = result.index(word) + 1
-
-    return answer
+        # product를 사용하여 알파벳 모음에서 길이 i의 모든 조합 생성
+        for per in product(li, repeat=i):
+            answer.append(''.join(per))  # 생성된 조합을 문자열로 변환하여 리스트에 추가
+    
+    answer.sort()  # 사전 순서대로 정렬
+    
+    # 주어진 단어 word가 정렬된 리스트 answer에서 몇 번째 위치하는지 반환
+    return answer.index(word) + 1  # 인덱스는 0부터 시작하므로 +1을 해서 1부터 시작하는 위치로 변환
