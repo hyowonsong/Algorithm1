b1545f41fb804cc8184537fcc57c4fb6fd7acc0b
hyowonsong <122877101+hyowonsong@users.noreply.github.com>
2024-07-09 22:31:07 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 171.78 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.py
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.py
@@ -1,10 +1,20 @@
-def solution(numbers):
-    stack = []
-    answer = [-1] * len(numbers)
+# 뒤에 있는 큰 수 찾기
+# 배열의 각 원소들에 대해 자신보다 뒤에 있는 숫자 중에서 
+# 자신보다 크면서 가장 가까이 있는 수 찾기
 
-    for i in range(len(numbers)):
-            while stack and numbers[stack[-1]] < numbers[i]:
-                answer[stack.pop()] = numbers[i]
-            stack.append(i)
+def solution(numbers):
+    n = len(numbers)
+    answer = [-1] * n  # 결과를 담을 리스트를 -1로 초기화
+    
+    stack = []  # 인덱스를 담을 스택
+    
+    for i in range(n):
+        # 스택이 비어있지 않고, 현재 원소가 스택의 맨 위 원소보다 크면
+        while stack and numbers[stack[-1]] < numbers[i]:
+            idx = stack.pop()  # 스택에서 원소를 꺼내어
+            answer[idx] = numbers[i]  # 해당 인덱스에 현재 원소를 뒷 큰수로 기록
+        
+        # 현재 원소의 인덱스를 스택에 추가
+        stack.append(i)
     
     return answer
