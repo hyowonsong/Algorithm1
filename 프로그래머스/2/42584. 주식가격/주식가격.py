c3a470a94fef4bb374d9b97179fd2a514ded9553
hyowonsong <122877101+hyowonsong@users.noreply.github.com>
2024-07-10 20:24:06 +0900

    [level 2] Title: 주식가격, Time: 14.59 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,20 +1,22 @@
-# 주식가격
-
 def solution(prices):
     n = len(prices)
-    # 배열 초기화
-    answer = [0]*n
+    answer = [0] * n  # 결과를 담을 리스트 초기화
+    stack = []  # 가격의 인덱스를 담을 스택
 
     for i in range(n):
-        for j in range(i+1, n):
-            # i가 j보다 크면 가격이 떨어진 것이므로 j-i 만큼의 시간 계산
-            if prices[i] > prices[j]:
-                answer[i] = j-i
-                break
-            # 가격이 떨어지지 않은 경우
-            elif j == len(prices) - 1:
-                answer[i] = j-i
-
+        # 스택이 비어있지 않고, 현재 가격이 스택의 맨 위 가격보다 작을 때
+        while stack and prices[stack[-1]] > prices[i]:
+            # 스택의 맨 위 가격 인덱스를 꺼내서
+            top = stack.pop()
+            # 현재 인덱스 i에서 스택의 맨 위 가격 인덱스 top을 빼면 가격이 떨어지지 않은 기간이 됨
+            answer[top] = i - top
+        
+        # 현재 가격의 인덱스를 스택에 추가
+        stack.append(i)
+    
+    # 마지막 가격은 가격이 떨어지지 않은 기간이 없으므로 0으로 설정
+    while stack:
+        top = stack.pop()
+        answer[top] = n - 1 - top
+    
     return answer
-
-
