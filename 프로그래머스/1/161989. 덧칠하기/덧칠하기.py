cb61c9e1b62b09409d97a6de6fa08f832abc1ce9
hyowonsong <122877101+hyowonsong@users.noreply.github.com>
2024-07-10 19:49:48 +0900

    [level 1] Title: 덧칠하기, Time: 3.02 ms, Memory: 11.8 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
@@ -1,13 +1,15 @@
 # 덧칠하기
 
 def solution(n, m, section):
-    answer = 1 # 칠하는 횟수
+    answer = 1 # 칠하는 횟수(맨처음 1로 설정)
     # 덧칠 시작점 맨처음에 정의해줘야(나중에 초기화 정의함)
     paint = section[0] 
-    # section의 길이만큼 for 문 돌아야
+    # section의 길이만큼 for 문 돌아야(1부터)
     for i in range(1, len(section)):
         if section[i] - paint >= m:
+            # 더 크다면 그냥 answer += 1 해준다. 다른 계산X
             answer += 1
-            paint = section[i]       # i로 초기해줘야 덧칠 시작점이 바뀌지
+            # i로 초기해줘야 덧칠 시작점이 바뀌지 안그러면 계속 section[0]
+            paint = section[i]  
             
     return answer
