b32174a34e501dda9873f4e3fd786fe2dd19a990
hyowonsong <122877101+hyowonsong@users.noreply.github.com>
2024-07-10 20:39:27 +0900

    [level 2] Title: 더 맵게, Time: 1679.10 ms, Memory: 51.9 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,17 +1,26 @@
-# 더 맵게 -> 힙 자료구조 이용
+import heapq 
 
-import heapq
-
-def solution(S, K):
-    heap = []
-    for i in S:
-        heapq.heappush(heap, i)
-
-    cnt = 0
-    while heap[0] < K:
-        heapq.heappush(heap, heapq.heappop(heap) + heapq.heappop(heap) * 2)
-        cnt += 1
-        
-        if len(heap) == 1 and heap[0] < K:
+def solution(scoville, K):
+    # 최소 힙 초기화
+    heapq.heapify(scoville)
+    
+    answer = 0
+    
+    while scoville[0] < K:
+        # 만약 힙의 길이가 1이고 첫번째 요소가 K보다 작다면
+        if len(scoville) == 1:
             return -1
-    return cnt
+        
+        # 가장 작은 두 요소 꺼내기
+        first = heapq.heappop(scoville)
+        second = heapq.heappop(scoville)
+        
+        # 새로운 음식의 스코빌 지수 계산
+        new_food = first + (second * 2)
+        
+        # 새로운 음식을 힙에 추가
+        heapq.heappush(scoville, new_food)
+        
+        answer += 1
+
+    return answer
