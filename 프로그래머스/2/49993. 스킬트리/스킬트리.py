71fe9ee9c0a65bcb03f1015225fb6950d7e2eea8
hyowonsong <122877101+hyowonsong@users.noreply.github.com>
2024-07-12 21:13:23 +0900

    [level 2] Title: 스킬트리, Time: 0.01 ms, Memory: 9.97 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.py
@@ -1,19 +1,24 @@
+# 스킬트리
+
 def solution(skill, skill_trees):
     answer = 0
 
     for skill_tree in skill_trees:
-        skill_index = 0  # 선행 스킬 순서의 현재 인덱스를 추적
+        # 순서를 index로 정의(이게 제일 중요!) 해주고 valid 도 정의 
+        index = 0
         valid = True
 
         for s in skill_tree:
             if s in skill:
-                if s == skill[skill_index]:
-                    skill_index += 1
+                # 만약 차례대로 안에 있으면 index += 1
+                if s == skill[index]:
+                    index += 1
                 else:
                     valid = False
                     break
 
+        # for 문 다 돌고 valid = True 면 answer += 1 
         if valid:
             answer += 1
-
+        
     return answer
