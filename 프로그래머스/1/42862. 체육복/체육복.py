5615ae8be6704230fb9f8bc7d0d231b820252465
hyowonsong <122877101+hyowonsong@users.noreply.github.com>
2024-07-14 21:30:59 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -0,0 +1,31 @@
+def solution(n, lost, reserve):
+    # 1은 체육복이 있음, 0은 없음을 의미
+    students = [1] * (n + 1)  # 0번 인덱스는 사용하지 않음
+    
+    # 도난당한 학생 처리
+    for l in lost:
+        students[l] -= 1
+    
+    # 여벌 체육복이 있는 학생 처리
+    for r in reserve:
+        students[r] += 1
+    
+    # 체육복 빌려주기
+    for i in range(1, n+1):
+        if students[i] == 0:
+            # 앞번호 학생에게 빌리기
+            if i > 1 and students[i-1] > 1:
+                students[i] += 1
+                students[i-1] -= 1
+            # 뒷번호 학생에게 빌리기
+            elif i < n and students[i+1] > 1:
+                students[i] += 1
+                students[i+1] -= 1
+    
+    # 체육복이 있는 학생 수 세기
+    count = 0
+    for i in range(1, n+1):
+        if students[i] > 0:
+            count += 1
+    
+    return count
