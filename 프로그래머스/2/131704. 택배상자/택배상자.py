55371ec74f907fab65c49cbde4aa9ccfa06d6e50
hyowonsong <122877101+hyowonsong@users.noreply.github.com>
2024-07-14 20:26:07 +0900

    [level 2] Title: 택배상자, Time: 129.85 ms, Memory: 53.1 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.py
+++ 프로그래머스/2/131704. 택배상자/택배상자.py
@@ -1,23 +1,27 @@
 def solution(order):
-    stack = []  
-    current_box = 1  # 현재 처리 중인 메인 컨테이너 벨트의 상자 번호
-    loaded = 0  # 트럭에 실은 상자의 수
-    
-    for next_box in order:  # order 리스트를 순회
-        # 현재 상자가 필요한 상자보다 작은 경우, 그 사이의 모든 상자를 스택에 추가
-        while current_box < next_box:
-            stack.append(current_box)
-            current_box += 1
-        
-        if current_box == next_box:  # 현재 상자가 필요한 상자인 경우
-            loaded += 1  # 트럭에 실음
-            current_box += 1  # 다음 상자로 이동
+    assist = []  
+    count = 0  
+    cur = 1  # 현재 메인 컨테이너 벨트에서 처리할 상자 번호
 
-        elif stack and stack[-1] == next_box:  # 스택의 맨 위 상자가 필요한 상자인 경우
-            stack.pop()  # 스택에서 상자를 꺼냄
-            loaded += 1  # 트럭에 실음
-            
-        else:  # 위의 조건을 모두 만족하지 않으면 더 이상 상자를 싣지 못함
-            break  # 반복 중단
-    
-    return loaded  # 트럭에 실은 총 상자 수 반환
+    # order 리스트의 각 상자 번호에 대해 반복
+    for box in order:  
+        # 현재 상자가 필요한 상자보다 작으면, 보조 벨트로 옮김
+        while cur < box:
+            assist.append(cur)
+            cur += 1
+        
+        # 현재 상자가 필요한 상자와 일치하면 
+        # 트럭에 싣고 카운트 증가 및 다음 상자로 이동
+        if cur == box:  
+            count += 1  
+            cur += 1  
+        # 보조 벨트 맨 위 상자가 필요한 상자라면
+        elif assist and assist[-1] == box:  
+            assist.pop()  
+            count += 1  
+        # 위 조건들에 해당하지 않으면 (즉, 필요한 상자를 찾을 수 없으면)
+        # 더 이상 진행 불가능하므로 반복 중단
+        else:  
+            break 
+
+    return count  
