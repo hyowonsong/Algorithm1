39cac59953b201eb35fede90bf920b4f98a27bc3
hyowonsong <122877101+hyowonsong@users.noreply.github.com>
2024-07-16 19:24:15 +0900

    [level 3] Title: 숫자 게임, Time: 56.41 ms, Memory: 18.6 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.py
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.py
@@ -1,16 +1,17 @@
 # B의 가장 큰 숫자가 A의 가장 큰 숫자를 이기는 선택
 
 def solution(A, B):
-    B.sort(reverse=True)  # B팀의 숫자들을 내림차순으로 정렬
-    b_index = 0  # B팀의 현재 인덱스
-    score = 0  # B팀의 점수
-
-    for a in sorted(A, reverse=True):  # A팀의 숫자들도 내림차순으로 정렬
-        if B[b_index] > a:
-            score += 1  # 점수를 증가
-            b_index += 1  # 다음 B팀 숫자로 이동
+    A.sort()
+    B.sort()
+    
+    a_idx = 0
+    b_idx = 0
+    answer = 0
+    
+    while b_idx < len(B) and a_idx < len(A):
+        if B[b_idx] > A[a_idx]:
+            answer += 1
+            a_idx += 1
+        b_idx += 1
         
-        if b_index == len(B):
-            break  # 모든 B팀 숫자를 사용했다면 종료
-
-    return score
+    return answer
