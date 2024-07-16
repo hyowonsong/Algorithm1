facb6a7fc4a62d3ffef316db23ab5196ea1c687d
hyowonsong <122877101+hyowonsong@users.noreply.github.com>
2024-07-16 20:00:34 +0900

    [level 3] Title: 단속카메라, Time: 0.81 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -1,14 +1,19 @@
-def solution(routes):
-    def point(route):
-        return route[1]
+def point(route):
+    return route[1]
 
-    routes.sort(key=point)  # 나간 지점을 기준으로 정렬
-    camera = -30001  # 카메라 위치 초기화 (가장 작은 진입/나간 지점보다 작은 값)
-    count = 0  # 설치된 카메라 수
+def solution(routes):
+    # 나간 지점을 기준으로 정렬
+    routes.sort(key=point)  
+    # 카메라 위치 초기화 (가장 작은 진입/나간 지점보다 작은 값)
+    camera = -30001  
+    # 설치된 카메라 수
+    count = 0  
 
     for route in routes:
-        if camera < route[0]:  # 현재 카메라가 해당 차량의 진입 지점보다 앞에 있으면
-            camera = route[1]  # 해당 차량의 나간 지점에 새 카메라 설치
+        # 현재 카메라가 해당 차량의 진입 지점보다 앞에 있으면
+        if camera < route[0]: 
+            # 해당 차량의 나간 지점에 새 카메라 설치
+            camera = route[1]  
             count += 1
 
     return count
