d768d1bf339954cfb6d54d806896cb2cd53a0954
hyowonsong <122877101+hyowonsong@users.noreply.github.com>
2024-07-21 14:22:59 +0900

    [level 2] Title: 소수 찾기, Time: 5.58 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -8,18 +8,24 @@ def solution(numbers):
             if n % i == 0:
                 return False
         return True
-
+    
     # 모든 가능한 숫자 조합 생성
     nums = set()
     for i in range(1, len(numbers) + 1):
+        # numbers에서 i개의 숫자를 선택하여 만들 수 있는 모든 순열 생성
         perms = permutations(numbers, i)
         for perm in perms:
+            # 각 순열을 정수로 변환하여 집합에 추가
+            # set을 사용하여 중복 제거
             nums.add(int(''.join(perm)))
 
     # 소수 개수 세기
     prime_count = 0
     for num in nums:
+        # 각 숫자에 대해 소수 여부 확인
         if is_prime(num):
+            # 소수일 경우 카운트 증가
             prime_count += 1
-    
+
+    # 찾은 소수의 개수 반환
     return prime_count
