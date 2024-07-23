b04c94a43087098db1238d72341b60854b8022d7
hyowonsong <122877101+hyowonsong@users.noreply.github.com>
2024-07-23 13:43:44 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -16,12 +16,14 @@ def solution(bridge_length, weight, truck_weights):
         # 그 트럭은 다리를 완전히 건넌 것이므로 다리에서 제거한다"
         if bridge_queue and bridge_queue[0][1] + bridge_length==time:
             total_weight -= bridge_queue.popleft()[0]
-        
-        # 다리에 트럭 올리기
+            
+        # 대기 중인 트럭 있고, 새 트럭을 올려도 무게 제한을 초과X
         if queue and total_weight + queue[0] <= weight:
-            # 대기 중인 트럭이 있고, 다리에 새 트럭을 올려도 무게 제한을 초과하지 않는 경우
-            truck = queue.popleft()  # 대기 큐에서 첫 번째 트럭을 꺼냄
-            bridge_queue.append((truck, time))  # 다리 큐에 (트럭 무게, 현재 시간) 튜플 추가
-            total_weight += truck  # 다리 위 총 무게 갱신
+            # 대기 큐에서 첫 번째 트럭을 꺼냄(truck 정의)
+            truck = queue.popleft()  
+            # 다리 큐에 (트럭 무게, 현재 시간) 튜플 추가
+            bridge_queue.append((truck, time))  
+            # 다리 위 총 무게 갱신(앞에서 정의한 truck 추가)
+            total_weight += truck 
     
     return time
