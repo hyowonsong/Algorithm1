167f639a13cabcebe5c5536eb692803d98a9d608
hyowonsong <122877101+hyowonsong@users.noreply.github.com>
2024-07-23 20:03:54 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.py
+++ 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.py
@@ -2,6 +2,6 @@ def solution(n):
     answer = ''
     while n > 0:
         n -= 1  # 1을 빼는 이유는 3으로 나눈 나머지 0, 1, 2를 1, 2, 4에 매핑하기 위함입니다.
-        answer = '124'[n % 3] + answer
-        n //= 3
+        answer = '124'[n % 3] + answer  # 나머지에 따라 1, 2, 4를 선택하여 문자열 앞에 추가합니다.
+        n //= 3  # n을 3으로 나누어 다음 자릿수로 넘어갑니다.
     return answer
