353ba183c5a53d258fe920bfbd92c100d5cf9e48
Hyowon Song <woniwoni30@gmail.com>
2024-07-26 11:09:01 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 0.37 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -1,6 +1,13 @@
+# 메뉴 리뉴얼
+
+# 단품 메뉴를 코스요리 형태로 재구성
+# 코스요리 메뉴는 최소 2가지 이상의 단품메뉴로 구성
+
 from itertools import combinations
 from collections import Counter
 
+# orders는 손님들의 주문 목록을 나타내며, 
+# course는 새로 만들고자 하는 코스 요리의 각 메뉴 수
 def solution(orders, course):
     answer = []
 
@@ -8,16 +15,18 @@ def solution(orders, course):
     for c in course:
         comb_counter = Counter()  # 조합의 빈도를 저장할 Counter 객체 생성
         
-        # 각 손님의 주문에 대해 처리
+        # 각 손님의 주문(order)을 알파벳 순서로 정렬
+        # 이렇게 하면 'ABC'와 'CBA' 같은 다른 순서의 같은 조합을 하나로 통일
         for order in orders:
-            sorted_order = sorted(order)  # 메뉴를 알파벳 순서로 정렬
+            sorted_order = sorted(order) 
             # 정렬된 주문으로부터 길이가 c인 모든 조합 생성 및 빈도 카운트
             comb_counter.update(combinations(sorted_order, c))
 
-        # 조합 중 가장 많이 함께 주문된 횟수 찾기
+        # comb_counter가 비어 있지 않다면, 
+        # 가장 많이 주문된 조합의 횟수를 max_count에 저장
         if comb_counter:
-            max_count = max(comb_counter.values())  # 가장 많이 주문된 횟수 찾기
-            # 가장 많이 주문된 횟수가 1보다 큰 경우만 고려
+            max_count = max(comb_counter.values())  
+            # max_count가 1보다 큰 경우(즉, 두 번 이상 주문된 경우)에만 계속 진행
             if max_count > 1:
                 # 각 조합과 그 조합의 주문 횟수에 대해 반복
                 for combo, count in comb_counter.items():
