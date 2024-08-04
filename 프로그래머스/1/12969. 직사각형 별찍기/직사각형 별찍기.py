fccd91b5b17104164d67b2f94443937889124579
Hyowon Song <woniwoni30@gmail.com>
2024-08-04 13:41:00 +0900

    [level 1] Title: 직사각형 별찍기, Time: 38.12 ms, Memory: 7.55 MB -BaekjoonHub

--- 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.py
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.py
@@ -1,6 +1,7 @@
-n, m = map(int,input().split())
+# 직사각형 별찍기
+n,m = map(int,input().split())
 
 for i in range(m):
     for j in range(n):
-        print('*', end='')
+        print('*',end = '')
     print()
