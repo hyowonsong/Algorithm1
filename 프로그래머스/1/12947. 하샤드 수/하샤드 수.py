71c212c43f1c9d7a2058f783eae888e2fb70578e
Hyowon Song <woniwoni30@gmail.com>
2024-08-04 12:47:29 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.py
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.py
@@ -1,10 +1,14 @@
-# 히샤드 수
-
 def solution(x):
+    # x의 자릿수의 합을 계산합니다.
+    new = str(x)
+    digit_sum = 0
 
-    new = sum(map(int,str(x)))
+    for digit in new:
+        digit_sum += int(digit)
     
-    if x % new == 0:
+    # x가 그 자릿수의 합으로 나누어지는지 확인합니다.
+    if x % digit_sum == 0:
         return True
     else:
         return False
+    
