f5f6a4280cb7e775a3dbf9a7e2fa2184e9c783f9
Hyowon Song <woniwoni30@gmail.com>
2024-08-04 12:12:33 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.py
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.py
@@ -2,8 +2,11 @@
 
 def solution(n):
     answer = 0
+    # str로 먼저 바꾸기
     new = str(n)
     
+    # new의 길이를 for 문 돌리기
     for i in range(len(new)):
+        # 여기서 int로 바꾸기 
         answer += int(new[i])
     return answer
