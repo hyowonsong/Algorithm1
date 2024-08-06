9242efa6e8dd79e60847bca534b0346434eb0334
Hyowon Song <woniwoni30@gmail.com>
2024-08-06 10:18:57 +0900

    [level 2] Title: 피보나치 수, Time: 34.77 ms, Memory: 13.6 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -1,5 +1,16 @@
+# 피보나치 수
+
 def solution(n):
-    fibo = [0,1]
-    for i in range(2,n+1):
-        fibo.append(fibo[i-1]+fibo[i-2])
-    return fibo[-1]%1234567
+    if n == 0:
+        return 0
+    
+    if n == 1:
+        return 1
+    
+    dp = [0] *(n+1)
+    dp[0] = 0
+    dp[1] = 1
+    
+    for i in range(2, n+1):
+        dp[i] = (dp[i-2]+ dp[i-1])%1234567
+    return dp[n]
