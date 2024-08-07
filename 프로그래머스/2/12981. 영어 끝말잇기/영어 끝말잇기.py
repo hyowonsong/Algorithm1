3eb15eb8465ac76a9d39fa95df3eb560f6b9eacf
Hyowon Song <woniwoni30@gmail.com>
2024-08-07 10:54:08 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.08 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -4,7 +4,8 @@ def solution(n, words):
     last_words = words[0][-1]
     number, order = 0, 0              # 여기 0, 0 이어야 된다.
     
-    for i in range(1,len(words)):      # 맨 처음에 0은 위에서 지정해줬으니 1부터
+    # 맨 처음에 0은 위에서 지정해줬으니 1부터
+    for i in range(1,len(words)):      
         if words[i] not in used_words and words[i][0] == last_words:
             used_words.append(words[i])
             last_words = words[i][-1]
