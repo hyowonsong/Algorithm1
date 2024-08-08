2b965d54ad7c0cf18d0d5ceff6becb72e9d0c524
Hyowon Song <woniwoni30@gmail.com>
2024-08-08 15:01:28 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,13 +1,18 @@
+# 의상
+
 def solution(clothes):
     answer = 0
-    hash_map ={}
-    for clothe, type in clothes:
-        # 키에 해당하는 값이 존재하면 그 값을 반환, 존재하지 않으면 기본값으로 0을 반환
-        hash_map[type] = hash_map.get(type, 0) + 1
+    hash_map = {}
 
-    
+    # 의상의 이름, 의상의 종류로 이루어짐
+    for clothe,type in clothes:
+        # hash_map.get(key, default)
+        # default는 키가 딕셔너리에 없을 때 반환하는 기본 값
+        hash_map[type] = hash_map.get(type,0) + 1
+
     answer = 1
-    # 여기서 입지 않는 경우를 추가해서 모든 조합 계산했어야
     for type in hash_map:
         answer *= hash_map[type] + 1
-    return answer - 1
+
+    # 모든 조합을 계산하고 아무것도 입지 않는 경우는 제외해야 하므로 최종 결과에서 1 빼기
+    return answer-1
