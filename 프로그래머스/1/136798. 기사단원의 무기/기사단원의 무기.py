667a227b69f672bd3a57f2093e7e346b9b38510e
Hyowon Song <woniwoni30@gmail.com>
2024-08-08 16:22:13 +0900

    [level 1] Title: 기사단원의 무기, Time: 914.42 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -1,17 +1,28 @@
+# 기사단원의 무기
+
 def solution(number, limit, power):
     answer = 0
-    divisor_count = [0] * (number + 1)  # 기사들의 약수 개수를 저장할 리스트 초기화
+
+    # 각 기사는 자신의 기사 번호의 약수 개수에 해당하는 공격력을 가진 무기 구매
+    # 제한수치보다 큰 공경력을 가진 무기를 구매해야 하는 기사는 기관에서 정한 무기 구매해야
+
+    def count_divisor(n):
+        count = 0 
+        for i in range(1,int(n**0.5)+1):
+            if n%i == 0:
+                count += 1
+                # i != n // i는 i가 n의 제곱근이 아닐 때 두 개의 약수 (i와 n // i)를 모두 계산하고, 
+                # 제곱근인 경우에는 중복 계산을 방지하기 위해 한 번만 계산
+                if i != n//i:
+                    count += 1
+        return count
     
-    # 각 기사들의 번호에 따른 약수의 개수 계산
-    for i in range(1, number + 1):
-        for j in range(i, number + 1, i):
-            divisor_count[j] += 1
-    
-    # 각 기사들이 구매할 무기의 공격력에 따라 필요한 철의 무게 계산
-    for count in divisor_count[1:]:
-        if count <= limit:  # 제한수치 이내의 공격력을 가진 무기를 구매할 경우
-            answer += count
-        else:  # 제한수치를 초과한 공격력을 가진 무기를 구매할 경우
-            answer += power
-            
-    return answer
+    total_weight = 0
+    for i in range(1,number+1):
+        divisors_count = count_divisor(i)
+        if divisors_count > limit:
+            total_weight += power
+        else:
+            total_weight += divisors_count
+
+    return total_weight
