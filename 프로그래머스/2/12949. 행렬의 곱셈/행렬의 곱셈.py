8a3f980910813afeca523638b730b97fc8ddc989
Hyowon Song <woniwoni30@gmail.com>
2024-08-08 14:37:48 +0900

    [level 2] Title: 행렬의 곱셈, Time: 122.08 ms, Memory: 11 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -1,18 +1,25 @@
+# 행렬의 곱셈
+
 def solution(arr1, arr2):
-    # arr1의 행의 개수 m, 열의 개수 n
+    # 2차원 행렬 arr1과 arr2 입력받아, arr1에 arr2를 곱한 결과를 반환하는 함수
+    # 행 먼저
     m = len(arr1)
+    # 열
     n = len(arr1[0])
-    
-    # arr2의 행의 개수는 n, 열의 개수는 p
+
+    # arr2의 행의 개수는 n, 열의 개수는 p (이거 해줘야)
     p = len(arr2[0])
-    
-    # 결과 행렬 C 초기화
-    result = [[0] * p for _ in range(m)]
-    
-    # 행렬 곱셈 계산
-    for i in range(m):       # C의 행 인덱스 i
-        for j in range(p):   # C의 열 인덱스 j
-            for k in range(n):  # A[i][k]와 B[k][j]를 곱해줌
+
+    # 행렬 초기화
+    result = [] 
+    for _ in range(m):
+        result.append([0]*p)
+
+    # 곱셈 수행
+    for i in range(m):
+        for j in range(p):
+            for k in range(n):
+                # 여기 +로 계속 누적해줘야
                 result[i][j] += arr1[i][k] * arr2[k][j]
-    
+
     return result
