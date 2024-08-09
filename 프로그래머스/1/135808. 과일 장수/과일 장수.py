dfc8321ea5e9a524b329c968e4b0b0b2ff03d758
Hyowon Song <woniwoni30@gmail.com>
2024-08-09 14:40:04 +0900

    [level 1] Title: 과일 장수, Time: 168.01 ms, Memory: 21.6 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -1,12 +1,14 @@
 # 과일 장수
 
-def solution(k, m, score):
-    answer = 0
-    score.sort(reverse = True)
+def solution(k,m,score):
+    answer= 0
+    # reverse는 원본 리스트 자체를 뒤집는다.
+    # reversed는 내장 함수로 원본 리스트는 변경되지 않습니다.
+    score.sort(reverse=True)
 
-     # m개씩 나누는거 생각해야지 (for 문 사용)
-    for i in range(0, len(score), m):
-        # score 길이: len(score) i:i+m 까지가 m 이면 
-        if len(score[i:i+m]) == m: 
+    # m개씩 포장
+    for i in range(0,len(score),m):
+        if len(score[i:i+m]) == m:
             answer += min(score[i:i+m])*m
+
     return answer
