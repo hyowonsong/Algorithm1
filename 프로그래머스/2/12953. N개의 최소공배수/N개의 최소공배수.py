d276bd93f5c608360a2a878926b43469564c9b10
Hyowon Song <woniwoni30@gmail.com>
2024-08-09 10:16:36 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -1,10 +1,14 @@
 import math
 
-def lcm(x, y):                           # 최소공배수 = 두수 곱한 것 나누기 최대공약수
-    return x * y // math.gcd(x, y)       # gcd = 최대공약수는 두 수의 공통된 약수 중 가증 큰 수
+# 최소공배수 = 두수 곱한 것 나누기 최대공약수
+def lcm(x, y):                 
+    # gcd = 최대공약수는 두 수의 공통된 약수 중 가증 큰 수
+    return x * y // math.gcd(x, y)       
 
 def solution(arr):
-    answer = arr[0]           # 초기값 answer를 설정해야 num과 비교 가능
+    # 초기값 answer를 설정해야 num과 비교 가능
+    answer = arr[0]       
+    # arr[1:] 부터 시작해서 answer와 num 중 최소공배수 체크
     for num in arr[1:]:
         answer = lcm(answer, num)       
     return answer
