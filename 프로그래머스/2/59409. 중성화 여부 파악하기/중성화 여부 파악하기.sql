82583f7bb5c93e9ed4a08274e2a27491055d96f4
Hyowon Song <woniwoni30@gmail.com>
2024-08-11 16:42:38 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -0,0 +1,13 @@
+# 보호소의 동물이 중성화되었는지 아닌지 파악
+# 중성화 동물은 컬럼에 Neutered, Spayed
+# 동물의 아이디, 이름, 중성화 여부를 아이디 순으로 조회
+
+
+SELECT ANIMAL_ID, NAME,  
+    CASE 
+        WHEN SEX_UPON_INTAKE LIKE '%Neutered%' OR SEX_UPON_INTAKE LIKE '%Spayed%' THEN 'O'
+        ELSE 'X'
+    END AS 중성화 
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID;
+
