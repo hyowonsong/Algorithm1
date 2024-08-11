4a8d9d397869898129a744515d18037fee1e3188
Hyowon Song <woniwoni30@gmail.com>
2024-08-11 16:59:21 +0900

    [level 2] Title: 입양 시각 구하기(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
@@ -0,0 +1,9 @@
+# 보호소에서 몇 시에 입양이 가장 활발하게 일어나는지 확인
+# 각 시간대별로 입양이 몇 건이나 발생했는지 조회하는 SQL문 작성
+
+-- 코드를 입력하세요
+SELECT HOUR(DATETIME) AS HOUR, COUNT(*) AS COUNT
+FROM ANIMAL_OUTS
+WHERE HOUR(DATETIME) BETWEEN 9 AND 19
+GROUP BY HOUR(DATETIME)
+ORDER BY HOUR(DATETIME)
