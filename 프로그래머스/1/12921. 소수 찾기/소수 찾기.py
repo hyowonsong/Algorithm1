b4280f7e7e09b5f76b1ecbd200c8ed833d845d47
Hyowon Song <woniwoni30@gmail.com>
2024-08-19 14:30:44 +0900

    [level 1] Title: 소수 찾기, Time: 3337.12 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -1,14 +1,14 @@
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
 def solution(n):
-    answer = 0  
-
-    for i in range(2, n+1):  # 2부터 n까지의 숫자에 대해 반복
-        is_prime = True  # 현재 숫자가 소수인지 여부를 판단
-        for j in range(2, int(i**0.5) + 1):  
-            if i % j == 0:  # i가 j로 나누어지면 소수가 아님
-                is_prime = False
-                break
-
-        if is_prime:  # is_prime이 True면 현재 숫자는 소수
-            answer += 1  # 소수 개수를 1 증가시킴
-
-    return answer 
+    count = 0
+    for i in range(2, n + 1):
+        if is_prime(i):
+            count += 1
+    return count
