d7b3217751d2a80940da2957897f7af9eae4dd34
Hyowon Song <woniwoni30@gmail.com>
2024-08-19 14:03:51 +0900

    [level 2] Title: 프로세스, Time: 1.08 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,30 +1,29 @@
-# 프로세스
 from collections import deque
 
 def solution(priorities, location):
-    answer = 0
-    queue = deque(priorities)
-
+    # 큐 초기화 (리스트를 하나씩 큐에 추가)
+    queue = deque()
+    for idx, value in enumerate(priorities):
+        queue.append((value, idx))
+    
+    order = 0
     
     while queue:
-        # max(queue) 를 지정해줘야 한다.
-        m = max(queue)
-        # 실행 대기 큐에서 대기중인 프로세스 하나를 꺼낸다.
-        l = queue.popleft()
-        # 문서 하나가 빠졌기 때문에 location -= 1 해준다.
-        location -= 1
-
-        # 큐에 대기중인 프로세스 중 우선순위가 더 높은 프로세스가 있다면 
-        # 꺼냈던 프로세스를 다시 넣고
-        # 사용자가 요청한 문서의 위치가 큐를 벗어나는 경우를 처리해준다.
-        if l != m:
-            queue.append(l)
-            if location < 0:
-                location = len(queue)- 1
-
-        else:
-            answer += 1
-            # location이 0 이하로 떨어진다면, 이는 큐의 왼쪽 끝을 벗어나는 것을 의미
-            if location < 0:
+        current = queue.popleft()
+        
+        # 큐에 있는 다른 프로세스 중 더 높은 우선순위가 있는지 확인
+        has_higher_priority = False
+        for i in queue:
+            if current[0] < i[0]:
+                has_higher_priority = True
                 break
-    return answer
+        
+        # 더 높은 우선순위가 있으면 다시 큐에 삽입
+        if has_higher_priority:
+            queue.append(current)  
+        # 더 높은 우선순위가 없고 current 가 가장 크다면 프로세스를 실행
+        else:
+            order += 1  
+            if current[1] == location:
+                return order  # 목표 프로세스가 실행될 순서를 반환
+                
