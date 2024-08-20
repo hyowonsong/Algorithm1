62df2a6da7f3e135a91e460ee1e86ad742168e92
Hyowon Song <woniwoni30@gmail.com>
2024-08-20 15:40:51 +0900

    [level 1] Title: 옹알이 (2), Time: 0.09 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
@@ -1,22 +1,19 @@
 def solution(babbling):
-    # 발음 가능한 단어 목록
     possible_words = ["aya", "ye", "woo", "ma"]
-    
-    # 발음할 수 있는 단어의 개수
+
     count = 0
-    
-    for word in babbling:
-        # 연속 발음을 방지하기 위해 같은 발음이 연속으로 있는지 확인
+
+    for bab in babbling:
         for pw in possible_words:
-            if pw * 2 in word:
+            # 같은 발음이 연속되는 경우는 제외
+            if pw*2 in bab:
                 break
-        else:
-            # 가능한 발음들을 반복하여 단어에서 제거
-            for pw in possible_words:
-                word = word.replace(pw, " ")
             
-            # 모든 가능한 발음이 제거된 후에 남는 글자가 없어야 정상 발음 가능
-            if word.strip() == "":
-                count += 1
-    
+            # 가능한 발음들을 반복하여 단어에서 제거
+            bab = bab.replace(pw, " ")
+
+        # 공백을 제거한 후, 문자열이 비어 있으면 유효한 발음
+        if bab.strip() == "":
+            count += 1
+
     return count
