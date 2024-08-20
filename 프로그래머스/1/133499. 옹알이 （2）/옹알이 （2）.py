af764eacfc6ba2858abfd6be0348f72292f3894a
Hyowon Song <woniwoni30@gmail.com>
2024-08-20 13:11:50 +0900

    [level 1] Title: 옹알이 (2), Time: 0.11 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
@@ -6,21 +6,17 @@ def solution(babbling):
     count = 0
     
     for word in babbling:
-        valid = True
-        previous = ""  # 이전 발음 저장
-        
+        # 연속 발음을 방지하기 위해 같은 발음이 연속으로 있는지 확인
         for pw in possible_words:
-            # 연속 발음을 방지하기 위해 같은 발음이 연속으로 있는지 확인
             if pw * 2 in word:
-                valid = False
                 break
-        
-        # 가능한 발음들을 반복하여 단어에서 제거
-        for pw in possible_words:
-            word = word.replace(pw, " ")
-        
-        # 모든 가능한 발음이 제거된 후에 남는 글자가 없어야 정상 발음 가능
-        if valid and word.strip() == "":
-            count += 1
+        else:
+            # 가능한 발음들을 반복하여 단어에서 제거
+            for pw in possible_words:
+                word = word.replace(pw, " ")
+            
+            # 모든 가능한 발음이 제거된 후에 남는 글자가 없어야 정상 발음 가능
+            if word.strip() == "":
+                count += 1
     
     return count
