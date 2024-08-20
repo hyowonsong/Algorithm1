d2668550c6b6877011c31b271d280f21a1d3c316
Hyowon Song <woniwoni30@gmail.com>
2024-08-20 11:43:09 +0900

    [level 1] Title: 덧칠하기, Time: 10.78 ms, Memory: 12.9 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
@@ -4,7 +4,7 @@ def solution(n, m, section):
     answer = 1 # 칠하는 횟수(맨처음 1로 설정)
     # 덧칠 시작점 맨처음에 정의해줘야(나중에 초기화 정의함)
     paint = section[0] 
-    # section의 길이만큼 for 문 돌아야(1부터)
+    # section[0] 같은 경우 먼저 정의 했으니까 1부터 시작
     for i in range(1, len(section)):
         if section[i] - paint >= m:
             # 더 크다면 그냥 answer += 1 해준다. 다른 계산X
