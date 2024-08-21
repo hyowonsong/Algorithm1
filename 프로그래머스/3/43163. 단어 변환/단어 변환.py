fac5f38f9055233f5aa4e046b8f878d0fc076693
Hyowon Song <woniwoni30@gmail.com>
2024-08-21 15:25:44 +0900

    [level 3] Title: 단어 변환, Time: 0.57 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,24 +1,27 @@
 from collections import deque
 
 def solution(begin, target, words):
+    # 변환할 수 없는 경우, 0 반환
     if target not in words:
         return 0
     
-    words_set = set(words)
-    queue = deque([(begin, 0)])
-    visited = set([begin])
+    # BFS를 위한 큐 초기화
+    queue = deque([(begin, 0)])  # (현재 단어, 변환 횟수)
     
     while queue:
-        current_word, level = queue.popleft()
+        current_word, current_count = queue.popleft()
         
+        # 현재 단어가 타겟 단어와 같다면 변환 횟수 반환
         if current_word == target:
-            return level
+            return current_count
         
-        for i in range(len(current_word)):
-            for c in 'abcdefghijklmnopqrstuvwxyz':
-                next_word = current_word[:i] + c + current_word[i+1:]
-                if next_word in words_set and next_word not in visited:
-                    visited.add(next_word)
-                    queue.append((next_word, level + 1))
+        # 단어 리스트를 순회하며 한 글자 차이나는 단어를 찾음
+        for word in words:
+            if sum([current_word[i] != word[i] for i in range(len(current_word))]) == 1:
+                queue.append((word, current_count + 1))
+                
+        # 이미 방문한 단어는 다시 방문하지 않기 위해 제거
+        words = [word for word in words if word != current_word]
     
+    # 변환할 수 없는 경우, 0 반환
     return 0
