4547139d1482f28aea02ba6e951e776941d8100f
Hyowon Song <woniwoni30@gmail.com>
2024-08-21 12:37:32 +0900

    [level 2] Title: 롤케이크 자르기, Time: 593.75 ms, Memory: 50.5 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -1,27 +1,28 @@
 def solution(topping):
     answer = 0
-    cheolsu = {}
+    cheoulsu = {}
     brother = {}
 
-    # 철수 세팅
+    # 처음엔 cheoulsu가 모든 토핑을 가지고 있다.
     for i in topping:
-        if i not in cheolsu:
-            cheolsu[i] = 1
+        if i in cheoulsu:
+            cheoulsu[i] += 1
         else:
-            cheolsu[i] += 1
+            cheoulsu[i] = 1
 
-    # 동생한테 하나씩 주면서 cheolsu는 하나씩 빼기
+    # 형이 하나씩 토핑을 가져감
     for i in topping:
-        cheolsu[i] -= 1
-        if cheolsu[i] == 0:
-            del cheolsu[i]
-        
-        if i not in brother:
-            brother[i] = 1
-        else:
+        cheoulsu[i] -= 1
+        if cheoulsu[i] == 0:
+            del cheoulsu[i]
+
+        if i in brother:
             brother[i] += 1
-        
-        if len(cheolsu) == len(brother):
+        else:
+            brother[i] = 1
+
+        # 현재 cheoulsu와 brother의 종류 개수를 비교
+        if len(cheoulsu) == len(brother):
             answer += 1
 
     return answer
