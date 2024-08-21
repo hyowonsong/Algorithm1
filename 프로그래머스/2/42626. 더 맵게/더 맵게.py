543df8d673aeb0d527a9c9f7193e3f1829ebff4f
Hyowon Song <woniwoni30@gmail.com>
2024-08-21 14:52:59 +0900

    [level 2] Title: 더 맵게, Time: 1690.32 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,26 +1,29 @@
-from heapq import heappop, heappush
+# 더 맵게
+from heapq import heappush, heappop
 
-def solution(scoville, K):
-    heap = []
+def solution(scoville, k):
     answer = 0
+    min_heap = []
 
     # 모든 스코빌 지수를 힙에 추가
     for s in scoville:
-        heappush(heap, s)
-    
+        heappush(min_heap, s)
+
     # 힙의 최소값이 K 이상이 될 때까지 반복
-    while heap[0] < K:
-        # 힙에 원소가 두 개 이상 있어야 함
-        if len(heap) < 2:
-            return -1
-        
-        # 가장 맵지 않은 두 개의 음식을 꺼내서 섞음
-        first = heappop(heap)
-        second = heappop(heap)
-        new_scoville = first + 2 * second
-        
-        # 새로 만든 음식을 힙에 추가
-        heappush(heap, new_scoville)
-        answer += 1
+    for s in scoville:
+        while min_heap[0] < k:
+            # 힙에 원소가 두 개 이상 있어야 함
+            if len(min_heap)<2:
+                return -1 
+            
+            else:
+                # 가장 맵지 않은 두 개의 음식을 꺼내서 섞음
+                first = heappop(min_heap)
+                second = heappop(min_heap)
+                new_scoville = first + 2* second
+
+                # 새로 만든 음식을 힙에 추가
+                heappush(min_heap, new_scoville)
+                answer += 1
 
     return answer
