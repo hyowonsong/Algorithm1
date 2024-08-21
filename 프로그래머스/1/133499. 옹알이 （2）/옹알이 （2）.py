579151a85a2b056d2a03d1e461d74d16525f1ab2
Hyowon Song <woniwoni30@gmail.com>
2024-08-21 11:52:58 +0900

    [level 1] Title: 옹알이 (2), Time: 0.14 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
@@ -8,9 +8,9 @@ def solution(babbling):
             # 같은 발음이 연속되는 경우는 제외
             if pw*2 in bab:
                 break
-            
-            # 가능한 발음들을 반복하여 단어에서 제거
-            bab = bab.replace(pw, " ")
+            else:
+                # 가능한 발음들을 반복하여 단어에서 제거
+                bab = bab.replace(pw, " ")
 
         # 공백을 제거한 후, 문자열이 비어 있으면 유효한 발음
         if bab.strip() == "":
