f88ccbfcf24d2dc4a567504e51ec0e09c41faeee
Hyowon Song <woniwoni30@gmail.com>
2024-08-25 18:35:39 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 4.66 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -1,37 +1,28 @@
 def solution(files):
-    def split_filename(filename):
-        head = ""
-        number = ""
-        tail = ""
+    # 파일명을 HEAD와 NUMBER로 파싱하는 함수
+    def parse_file(file):
+        head = ''   # HEAD 부분을 저장할 변수
+        number = '' # NUMBER 부분을 저장할 변수
+        i = 0
+        length = len(file)
         
-        # HEAD 부분 찾기
-        for i, char in enumerate(filename):
-            if char.isdigit():
-                break
-            head += char
+        # HEAD 추출: 숫자가 나오기 전까지 문자를 계속 추가
+        while i < length and not file[i].isdigit():
+            head += file[i]
+            i += 1
         
-        # NUMBER 부분 찾기
-        for char in filename[i:]:
-            if not char.isdigit():
-                break
-            if len(number) >= 5:
-                break
-            number += char
+        # NUMBER 추출: 숫자가 나오는 부분부터 최대 5자리까지 추출
+        while i < length and file[i].isdigit():
+            number += file[i]
+            i += 1
         
-        # TAIL은 나머지 부분
-        tail = filename[i + len(number):]
-        
-        return head, number, tail
-
-    def compare(file_info):
-        head, number, _, index = file_info
-        return (head.lower(), int(number), index)
-
-    # 파일명을 분리하고 원래 인덱스와 함께 저장
-    file_infos = [(split_filename(f) + (i,)) for i, f in enumerate(files)]
+        # TAIL은 정렬에 필요 없으므로 추출하지 않음
+        return (head, number)
     
-    # 정렬
-    sorted_files = sorted(file_infos, key=compare)
+    # 정렬을 위한 키 함수
+    def sort_key(file):
+        head, number = parse_file(file)
+        return (head.lower(), int(number))
     
-    # 정렬된 순서대로 원래 파일명 반환
-    return [files[info[3]] for info in sorted_files]
+    # 파일명 리스트를 정렬 기준에 맞게 정렬하여 반환
+    return sorted(files, key=sort_key)
