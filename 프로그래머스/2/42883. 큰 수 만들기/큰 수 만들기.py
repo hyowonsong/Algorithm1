30310cb9d7da44d69d51b7276d5bbc44317509b9
Hyowon Song <woniwoni30@gmail.com>
2024-08-28 13:48:39 +0900

    [level 2] Title: 큰 수 만들기, Time: 108.44 ms, Memory: 13 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -1,21 +1,20 @@
 def solution(number, k):
     stack = []  # 결과를 저장할 스택 초기화
     
-    for num in number:
-        # k > 0: 아직 제거할 숫자가 남아있음
-        # stack: 스택이 비어있지 않음
-        # stack[-1] < num: 스택의 마지막 숫자가 현재 숫자보다 작음
-        while k > 0 and stack and stack[-1] < num:
+    for i in range(len(number)):
+        # i는 단순한 인덱스 번호로, 리스트의 실제 값이 아님
+        # stack[-1] < i는 의미가 없음
+        while k > 0 and stack and stack[-1] < number[i]:  # number[i]를 사용해야 함
             stack.pop()  # 스택에서 마지막 숫자 제거
             k -= 1  # 제거한 숫자 개수 감소
         
-        # stack 에 num 추가
-        stack.append(num) 
+        # stack에 현재 숫자 추가
+        stack.append(number[i])  # number[i]를 사용해야 함
     
     # 모든 숫자를 순회한 후에도 제거할 숫자가 남아있다면
     if k > 0:
-        # 끝에서 k번째 요소를 제외한 모든 요소를 포함
+        # stack의 처음부터 len(stack) - k번째 요소까지를 포함
         stack = stack[:-k]  
     
     # 스택의 숫자들을 문자열로 변환하여 반환
-    return ''.join(stack)  
+    return ''.join(stack)
