ecba8fef00881f1a6fa9fe6a4e3b4d33a8b149f2
Hyowon Song <woniwoni30@gmail.com>
2024-08-28 14:06:13 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.89 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
@@ -1,6 +1,6 @@
 def solution(board, moves):
     # 바구니를 초기화
-    basket = []
+    stack = []
     # 인형의 개수를 저장할 변수
     count = 0
     
@@ -17,13 +17,13 @@ def solution(board, moves):
                 doll = board[row][col]
                 # 해당 위치를 빈 칸으로 설정
                 board[row][col] = 0
-                # 바구니에 인형 추가
+                # 스택에 인형 추가
                 # 마지막 인형과 같은 경우 터트리기
-                if basket and basket[-1] == doll:
-                    basket.pop()  
+                if stack and stack[-1] == doll:
+                    stack.pop()  
                     count += 2  # 인형 두 개의 개수를 더함
                 else:
-                    basket.append(doll)
+                    stack.append(doll)
                 break
     
     return count
