7456749e1a85028d00164e2df22c131de39486b5
Hyowon Song <woniwoni30@gmail.com>
2024-08-31 11:25:11 +0900

    [Bronze III] Title: 이진수, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/3460. 이진수/이진수.py
+++ 백준/Bronze/3460. 이진수/이진수.py
@@ -1,14 +1,14 @@
-def find_one_positions(n):
-    positions = []
-    position = 0
-    while n > 0:
-        if n & 1:  # 최하위 비트가 1인지 확인
-            positions.append(str(position))
-        n >>= 1  # 오른쪽으로 1비트 시프트
-        position += 1
-    return ' '.join(positions)
+T = int(input())
 
-T = int(input())  # 테스트 케이스 개수
 for i in range(T):
     n = int(input())
-    print(find_one_positions(n))
+    positions = []
+    position = 0
+
+    while n>0:
+        if n & 1:
+            positions.append(position)
+        n >>= 1
+        position += 1
+
+    print(' '.join(map(str,positions)))
