72c944a44ad7fc2d99ec1905afe30bc20355f874
Hyowon Song <woniwoni30@gmail.com>
2024-09-08 14:46:29 +0900

    [level 2] Title: 올바른 괄호, Time: 14.18 ms, Memory: 52.6 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/README.md
+++ 프로그래머스/2/12909. 올바른 괄호/README.md
@@ -1,10 +1,10 @@
 # [level 2] 올바른 괄호 - 12909 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909?language=java) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 7.46 ms
+메모리: 52.6 MB, 시간: 14.18 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 02월 27일 14:22:29
+2024년 09월 08일 14:46:25
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,21 @@
+import java.util.ArrayDeque;
+
+class Solution {
+    boolean solution(String s) {
+        ArrayDeque<Character> stack = new ArrayDeque<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                // 열린 괄호는 스택에 넣음
+                stack.push(c);
+            } else if (c == ')') {
+                // 닫힌 괄호는 스택에서 열린 괄호를 꺼냄
+                if (stack.isEmpty() || stack.pop() != '(') {
+                    return false; // 스택이 비었거나 짝이 맞지 않으면 false
+                }
+            }
+        }
+        // 모든 괄호가 짝지어졌으면 스택이 비어 있어야 함
+        return stack.isEmpty();
+    }
+}
