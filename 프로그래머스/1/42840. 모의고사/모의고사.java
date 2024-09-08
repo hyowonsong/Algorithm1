37dc05e51b3018e65cdd5ae4874ae0fa7c6fc322
Hyowon Song <woniwoni30@gmail.com>
2024-09-08 11:08:25 +0900

    [level 1] Title: 모의고사, Time: 0.79 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -0,0 +1,46 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public int[] solution(int[] answers) {
+        // 1번, 2번, 3번 수포자의 답안 패턴 정의
+        int[] pattern1 = {1, 2, 3, 4, 5};
+        int[] pattern2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] pattern3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+
+        // 맞힌 문제 개수를 저장할 배열
+        int[] score = {0, 0, 0};
+
+        // answers 배열과 비교하며 각 수포자의 맞힌 개수 계산
+        for (int i = 0; i < answers.length; i++) {
+            if (answers[i] == pattern1[i % pattern1.length]) {
+                score[0]++;
+            }
+            if (answers[i] == pattern2[i % pattern2.length]) {
+                score[1]++;
+            }
+            if (answers[i] == pattern3[i % pattern3.length]) {
+                score[2]++;
+            }
+        }
+
+        // 가장 높은 점수를 찾음
+        int maxScore = Math.max(score[0], Math.max(score[1], score[2]));
+
+        // 가장 높은 점수를 받은 사람을 리스트에 추가
+        List<Integer> topScorers = new ArrayList<>();
+        for (int i = 0; i < score.length; i++) {
+            if (score[i] == maxScore) {
+                topScorers.add(i + 1); // 수포자는 1번부터 시작하므로 인덱스에 +1
+            }
+        }
+
+        // 리스트를 배열로 변환하여 반환
+        int[] result = new int[topScorers.size()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = topScorers.get(i);
+        }
+        return result;
+    }
+}
