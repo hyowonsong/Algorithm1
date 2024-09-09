73ddcb82ac6381d03c21f599332e426e2ed46689
Hyowon Song <woniwoni30@gmail.com>
2024-09-09 15:35:06 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 5.15 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -9,14 +9,18 @@ class Solution {
         // TreeSet 은 평균적으로  O(logN). 데이터 순서 유지 + 집합 특성 유지할 때
         HashSet<Integer> set = new HashSet<>();
 
-        // 두 수를
+        // 두 수를 더한 결과를 해시셋에 추가
         for (int i=0; i<numbers.length-1; i++){
             for (int j = i+1; j < numbers.length; j++){
                 set.add(numbers[i] + numbers[j]);
             }
         }
 
-        //
+        // 해시셋의 값을 오름차순 정렬하고 int[] 형태의 배열로 변환하여 반환
+        // mapToInt() 는 각 Integer 객체를 기본형 int 로 변환하는 함수입니다.
+        // Integer::intValue 는 Integer 객체의 intValue()메서드를 참조하여 int 값 얻습니다.
+        // toArray() 는 IntStream 의 모든 요소를 int[] 배열로 변환합니다. 
+        // toArray() 메서드는 스트림의 요소를 배열로 변환하는데 사용합니다. 
         return set.stream().sorted().mapToInt(Integer::intValue).toArray();
     }
 }
