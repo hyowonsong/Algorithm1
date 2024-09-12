0971e9afe177299d7752111819347ff8ae1d788c
Hyowon Song <woniwoni30@gmail.com>
2024-09-12 15:37:48 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.14 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,9 +1,10 @@
-// 1. 시간복잡도 : N은 words 의 길이.
-// 2. words 의 길이만큼 반복 순회하고 각 연산 시간 복잡도는 O(1)이니까 O(N) 가능할듯!
 
 import java.util.HashSet;
+// 영어 끝말잇기
 
-class Solution {
+// 1. 시간복잡도 : N은 words 의 길이.
+// 2. words 의 길이만큼 반복 순회하고 각 연산 시간 복잡도는 O(1)이니까 O(N) 가능할듯!
+public class Solution {
     public int[] solution(int n, String[] words) {
         // 이미 사용한 단어를 저장하는 set
         HashSet<String> usedWords = new HashSet<>();
@@ -12,6 +13,8 @@ class Solution {
 
         for (int i = 0; i < words.length; i++) {
             // 이미 사용한 단어이거나 첫 글자가 이전 단어와 일치하지 않으면
+            // 이미 사용한 단어라면 python 에서는 in 이지만
+            // 여기서는 contains 를 사용한다.
             if (usedWords.contains(words[i]) || words[i].charAt(0) != prevWord) {
                 // 탈락하는 사람의 번호와 차례를 반환
                 return new int[]{(i % n) + 1, (i / n) + 1};
