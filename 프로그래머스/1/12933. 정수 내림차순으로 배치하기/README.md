1e069970e055a47fa3b97ef8e17728dc30b29970
Hyowon Song <woniwoni30@gmail.com>
2024-09-20 13:18:38 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.67 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/README.md
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 정수 내림차순으로 배치하기 - 12933 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12933?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12933) 
 
 ### 성능 요약
 
-메모리: 72.5 MB, 시간: 0.98 ms
+메모리: 75.7 MB, 시간: 0.67 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 17일 23:21:05
+2024년 09월 20일 13:18:34
 
 ### 문제 설명
 
--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -2,21 +2,29 @@ import java.util.Arrays;
 import java.util.Collections;
 
 class Solution {
+    // 입력을 보면 80억까지 들어올 수 있기 때문에 long 사용
     public long solution(long n){
         // 정수 n 을 문자열로 변환하고 각 자릿수를 배열로 저장
+        // valueof 는 n 이 어떤 타입이든 문자열로 변환해줍니다.
         String[] digits = String.valueOf(n).split("");
 
         // 내림차순으로 정렬
         Arrays.sort(digits, Collections.reverseOrder());
 
+
         // 정렬된 숫자를 다시 하나의 문자열로 합칩
         StringBuilder sb = new StringBuilder();
-        for (String digit: digits){
+        for (String digit : digits){
             sb.append(digit);
         }
-
-        // 문자열을 long 으로 변환하여 반환
+        
         // 문자열을 Long.parseLong() 메서드를 이용하여 long 형으로 변환하여 반환
+        // StringBuilder 에 저장된 숫자를 문자열로 변환 후, 그 문자열을 다시 long 타입 변환
+        // StringBuilder 의 내용은 문자열로 표현되지만, 그 자체로는 문자열이 아닙니다.
+        // 문자열을 더할 때 새로운 객체를 생성하는 것이 아니라 기존의 데이터에 더하는 방식이기 때문
+        // int 는 4 바이트(32비트) long 은 64비트
+        // int 타입의 숫자가 long 타입으로 변환된다면, 이 과정은 자동형변환
+        // long 을 int 로 변환할 때는 명시적인 형 변환
         return Long.parseLong(sb.toString());
     }
 }
