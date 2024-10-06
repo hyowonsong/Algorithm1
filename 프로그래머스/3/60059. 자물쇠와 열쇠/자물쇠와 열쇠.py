f713de3da5a58f7da5b8a5a4f2f373a972266665
Hyowon Song <woniwoni30@gmail.com>
2024-10-06 20:51:26 +0900

    [level 3] Title: 자물쇠와 열쇠, Time: 580.43 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.py
+++ 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.py
@@ -1,43 +1,38 @@
-def match(arr, key, rot, r, c):
-    n = len(key)
+def rotate(a):
+    n = len(a) 
+    m = len(a[0]) 
+    result = [[0] * n for _ in range(m)]
     for i in range(n):
-        for j in range(n):
-            if rot == 0:  # 만약 회전이 없다면, 키를 그대로 더합니다
-                arr[r + i][c + j] += key[i][j]
-            elif rot == 1:
-                arr[r + i][c + j] += key[n - 1 - j][i]  # 90도 회전
-            elif rot == 2:
-                arr[r + i][c + j] += key[n - 1 - i][n - 1 - j]  # 180도 회전
-            else:
-                arr[r + i][c + j] += key[j][n - 1 - i]  # 270도 회전
+        for j in range(m):
+            result[j][n - i - 1] = a[i][j]
+    return result
 
-def check(arr, offset, n):
-    for i in range(n):
-        for j in range(n):
-            if arr[offset + i][offset + j] != 1:
-                return False  # 자물쇠 부분 중 하나라도 맞지 않으면 False 반환
-    return True  # 모든 부분이 1로 맞다면 True 반환
+def check(new_lock):
+    lock_length = len(new_lock) // 3
+    for i in range(lock_length, lock_length * 2):
+        for j in range(lock_length, lock_length * 2):
+            if new_lock[i][j] != 1:
+                return False
+    return True
 
 def solution(key, lock):
-    offset = len(key) - 1
-    # 자물쇠를 (1,1)에서 떨어지게 놔두는데 그 거리 만큼을 offset이라고 한다
+    n = len(lock)
+    m = len(key)
+    new_lock = [[0] * (n * 3) for _ in range(n * 3)]
+    for i in range(n):
+        for j in range(n):
+            new_lock[i + n][j + n] = lock[i][j]
 
-    # 행, 열의 좌표를 r, c로 설정하고 범위는 offset + 자물쇠 길이
-    for r in range(offset + len(lock)):
-        for c in range(offset + len(lock)):
-            # 열쇠를 돌려보는 것(4개 방향으로 돌린다.)
-            for rot in range(4):
-                # 2차원 배열 초기값을 설정합니다.
-                arr = [[0 for _ in range(58)] for _ in range(58)]
-
-                # 자물쇠를 복사해줍니다.
-                for i in range(len(lock)):
-                    for j in range(len(lock)):
-                        arr[offset + i][offset + j] = lock[i][j]
-
-                match(arr, key, rot, r, c)
-                # 만약 check 함수가 True를 반환하면 열쇠가 맞아떨어진 경우이므로 True 반환
-                if check(arr, offset, len(lock)):
+    for rotation in range(4):
+        key = rotate(key) 
+        for x in range(n * 2):
+            for y in range(n * 2):
+                for i in range(m):
+                    for j in range(m):
+                        new_lock[x + i][y + j] += key[i][j]
+                if check(new_lock) == True:
                     return True
-
-    return False  # 모든 경우를 확인해도 맞는 열쇠를 찾을 수 없으면 False 반환
+                for i in range(m):
+                    for j in range(m):
+                        new_lock[x + i][y + j] -= key[i][j]
+    return False
