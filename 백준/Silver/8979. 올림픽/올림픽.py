2d9e53aee8812d2b712a0fb6f4f308a35d326769
Hyowon Song <woniwoni30@gmail.com>
2024-10-08 23:26:41 +0900

    [Silver V] Title: 올림픽, Time: 56 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -1,22 +1,32 @@
-# 입력: 국가 수 N과 순위를 알고 싶은 국가 K
+# N: 국가의 수, K: 등수를 알고 싶은 국가
 N, K = map(int, input().split())
 
-# 국가와 메달 정보를 저장할 리스트
-medals = []
+# 각 국가의 메달 정보를 저장할 리스트
+countries = []
 
-# 각 국가의 메달 정보 입력 받기
+# N개의 국가 정보 입력 받기
 for _ in range(N):
+    # country: 국가번호, gold: 금메달, silver: 은메달, bronze: 동메달
     country, gold, silver, bronze = map(int, input().split())
-    medals.append((country, gold, silver, bronze))
+    countries.append([country, gold, silver, bronze])
 
-# 메달 리스트를 금, 은, 동메달 순으로 내림차순 정렬
-medals.sort(key=lambda x: (-x[1], -x[2], -x[3]))
-
-# K번 국가의 등수 구하기
-for i in range(N):
-    if medals[i][0] == K:
-        # 메달 수가 동일한 다른 국가들의 공동 순위를 고려해 처리
-        rank = i + 1
+# K번 국가의 메달 정보 찾기
+k_medals = None
+for country in countries:
+    if country[0] == K:
+        k_medals = country[1:]  # 금, 은, 동메달 정보만 저장
         break
 
-print(rank)
+# K국가보다 더 잘한 나라의 수를 세기
+better_count = 0
+for country in countries:
+    medals = country[1:]  # 현재 국가의 메달 정보
+    
+    # 메달 순서대로 비교 (금 -> 은 -> 동)
+    if (medals[0] > k_medals[0]) or \
+       (medals[0] == k_medals[0] and medals[1] > k_medals[1]) or \
+       (medals[0] == k_medals[0] and medals[1] == k_medals[1] and medals[2] > k_medals[2]):
+        better_count += 1
+
+# K국가의 등수 = (자신보다 더 잘한 나라 수) + 1
+print(better_count + 1)
