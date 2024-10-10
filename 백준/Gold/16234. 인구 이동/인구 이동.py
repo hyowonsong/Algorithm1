d77e0e7bf1402cf5ade6a82a3e53da6e2213b1bf
Hyowon Song <woniwoni30@gmail.com>
2024-10-10 19:52:23 +0900

    [Gold IV] Title: 인구 이동, Time: 5560 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.py
+++ 백준/Gold/16234. 인구 이동/인구 이동.py
@@ -1,19 +1,21 @@
-
 from collections import deque
 
 # BFS 탐색을 위한 함수 정의
 def bfs(si, sj):
     q = deque()
 
-    # 초기 상태 설정
-    q.append((si, sj))      # 큐에 시작 좌표 삽입
-    v[si][sj] = 1           # 방문 표시
-    alst = [(si, sj)]       # 연합에 포함된 나라 리스트
-    sm = arr[si][sj]        # 연합의 인구 총합
+    # 큐에 시작 좌표 삽입
+    q.append((si, sj))    
+    # 방문 표시  
+    v[si][sj] = 1           
+    # 연합에 포함된 나라 리스트
+    alst = [(si, sj)]       
+    # 연합의 인구 총합
+    total = arr[si][sj]        
 
     # 네 방향 이동을 나타내는 리스트
-    dx = [-1, 0, 1, 0]  # 상, 하, 좌, 우 방향
-    dy = [0, 1, 0, -1]  # 상, 하, 좌, 우 방향
+    dx = [-1, 0, 1, 0]  
+    dy = [0, 1, 0, -1]  
 
     # BFS 탐색
     while q:
@@ -27,35 +29,40 @@ def bfs(si, sj):
                 q.append((nx, ny))    # 큐에 추가
                 v[nx][ny] = 1         # 방문 처리
                 alst.append((nx, ny)) # 연합 리스트에 추가
-                sm += arr[nx][ny]     # 인구 총합에 더함
+                total += arr[nx][ny]     # 인구 총합에 더함
     
     # 연합이 형성된 경우 (2개 이상의 나라로 구성된 경우)
     if len(alst) > 1:
         for ti, tj in alst:
-            arr[ti][tj] = sm // len(alst)  # 연합에 속한 나라의 인구를 평균값으로 설정
+            arr[ti][tj] = total // len(alst)  # 연합에 속한 나라의 인구를 평균값으로 설정
         return 1  # 연합이 있었음을 나타냄
     return 0  # 연합이 없었음을 나타냄
+
 
 # 입력 처리
 N, L, R = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
 
-ans = 0  # 인구 이동 횟수
+ans = 0
 while True:
     # 방문 여부를 확인하기 위한 2차원 리스트 초기화
     v = [[0] * N for _ in range(N)]
-    flag = 0  # 연합이 발생했는지 여부를 저장
+
+    # 연합이 발생했는지 여부를 저장
+    flag = 0 
 
     # 모든 나라를 탐색하며 연합 형성
     for i in range(N):
         for j in range(N):
-            if v[i][j] == 0:  # 아직 방문하지 않은 나라에 대해
-                flag = max(flag, bfs(i, j))  # BFS 탐색을 수행하고 연합이 있었다면 1 반환
+            # 아직 방문하지 않은 나라에 대해
+            if v[i][j] == 0:  
+                # BFS 탐색을 수행하고 연합이 있었다면 1 반환
+                flag = max(flag, bfs(i, j))  
     
     # 연합이 발생하지 않았으면 종료
     if flag == 0:
         break
-    ans += 1  # 연합이 발생한 경우 인구 이동 횟수 증가
+    # 연합이 발생한 경우 인구 이동 횟수 증가
+    ans += 1  
 
-# 결과 출력
 print(ans)
