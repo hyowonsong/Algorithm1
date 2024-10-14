b4720d11511af35c1406a8652be3fe5beb344f8a
Hyowon Song <woniwoni30@gmail.com>
2024-10-14 15:02:38 +0900

    [Silver III] Title: 바이러스, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,16 +1,29 @@
+# 바이러스
+n = int(input())  
+v = int(input()) 
 
-n=int(input()) # 컴퓨터 개수
-v=int(input()) # 연결선 개수
-graph = [[] for i in range(n+1)] # 그래프 초기화
-visited=[0]*(n+1) # 방문한 컴퓨터인지 표시
-for i in range(v): # 그래프 생성
-    a,b=map(int,input().split())
-    graph[a]+=[b] # a에 b 연결
-    graph[b]+=[a] # b에 a 연결 -> 양방향
+# 그래프 초기화 (n+1 크기로 설정, 1번부터 시작하기 때문)
+graph = [[0] * (n+1) for _ in range(n+1)]
+
+for i in range(v):
+    a, b = map(int, input().split())
+    graph[a][b] = 1
+    graph[b][a] = 1
+
+visited = [0] * (n+1)
+
+# DFS 함수
 def dfs(v):
-    visited[v]=1
-    for nx in graph[v]:
-        if visited[nx]==0:
-            dfs(nx)
-dfs(1)
-print(sum(visited)-1)
+    visited[v] = 1  # 현재 노드 방문 처리
+    count = 1  # 현재 노드도 감염된 컴퓨터로 포함
+    for i in range(1, n+1):
+        if not visited[i] and graph[v][i] == 1:  # 아직 방문하지 않았고, 연결된 경우
+            count += dfs(i)  # 연결된 컴퓨터도 감염시키고 카운트 증가
+    return count
+
+# 1번 컴퓨터를 제외한 감염된 컴퓨터 수를 반환해야 하므로 -1
+result = dfs(1) - 1
+print(result)
+
+
+
