3507ae4b627595e7abe732edda7d0f5daaf5fc33
Hyowon Song <woniwoni30@gmail.com>
2024-10-16 16:28:54 +0900

    [Unrated] Title: [모의 SW 역량테스트] 숫자 만들기, Time: 326 ms, Memory: 56,904 KB -BaekjoonHub

---
+++ SWEA/Unrated/4008. ［모의 SW 역량테스트］ 숫자 만들기/［모의 SW 역량테스트］ 숫자 만들기.py
@@ -0,0 +1,26 @@
+def dfs(n, num, add, sub, mul, div):
+    global mn,mx
+    if n == N:
+        mn = min(mn,num)
+        mx = max(mx, num)
+        return
+    
+    if add : dfs(n+1, num+lst[n], add-1, sub, mul, div)
+    if sub : dfs(n+1, num-lst[n], add, sub-1, mul, div)
+    if mul : dfs(n+1, num*lst[n], add, sub, mul-1, div)
+    if div : dfs(n+1, int(num/lst[n]), add, sub, mul,div-1)
+
+# 총 테스트 케이스의 개수
+T = int(input())
+for test_case in range(1, T+1):
+    # 첫 번째 테스트 케이스
+    N = int(input())
+    # 각 연산자
+    add, sub, mul, div = map(int, input().split())
+    lst = list(map(int, input().split()))
+
+    mn = int(1e8)
+    mx = int(-1e8)
+    dfs(1,lst[0], add, sub, mul, div)
+
+    print(f'#{test_case} {mx-mn}')
