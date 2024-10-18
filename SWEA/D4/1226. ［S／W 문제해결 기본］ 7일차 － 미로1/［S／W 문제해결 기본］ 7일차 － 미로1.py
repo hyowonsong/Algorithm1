85af477ff72934b2a68fb40d094000ae9bfddc60
Hyowon Song <woniwoni30@gmail.com>
2024-10-18 16:51:00 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로1, Time: 124 ms, Memory: 49,028 KB -BaekjoonHub

---
+++ SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.py
@@ -0,0 +1,38 @@
+def bfs(si, sj):
+    # [1] q, v[] 생성
+    q = []
+    v = [[0]*N for _ in range(N)]
+
+    # [2] q에 초기데이터 삽입, v표시
+    q.append((si,sj))
+    v[si][sj]=1
+
+    while q:
+        ci,cj = q.pop(0)
+        if arr[ci][cj]==3:
+            return v[ci][cj]
+
+        # 네방향, 범위내, 미방문, 벽이아니면 방문(q)
+        for di,dj in ((-1,0),(1,0),(0,-1),(0,1)):
+            ni,nj = ci+di, cj+dj
+            if 0<=ni<N and 0<=nj<N and v[ni][nj]==0 and arr[ni][nj]!=1:
+                q.append((ni,nj))
+                v[ni][nj]=1
+    return 0
+
+
+
+T = 10
+for test_case in range(1, T + 1):
+    _ = input()
+    N = 16
+    arr = [list(map(int,input())) for _ in range(N)]
+    for i in range(N):
+        for j in range(N):
+            if arr[i][j]==2:
+                si, sj = i, j
+            if arr[i][j]==3:
+                ei, ej = i, j
+
+    ans = bfs(si, sj)
+    print(f'#{test_case} {ans}')
