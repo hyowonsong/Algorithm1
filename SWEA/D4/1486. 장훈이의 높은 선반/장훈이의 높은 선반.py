088d01adf618fb3024de32d53b0ebbf0fb2a5b25
Hyowon Song <woniwoni30@gmail.com>
2024-10-28 21:23:19 +0900

    [D4] Title: 장훈이의 높은 선반, Time: 211 ms, Memory: 51,580 KB -BaekjoonHub

--- SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.py
+++ SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.py
@@ -1,22 +1,22 @@
 def dfs(n, sm):
     global ans
-    if n == N:
-        if sm>=B:
+ 
+    if ans <= sm-B:
+        return
+ 
+    if n==N:
+        if sm >= B:
             ans = min(ans, sm-B)
         return
-    
-    # 포함하는 경우
-    dfs(n+1, sm+lst[n])
-    # 포함하지 않는 경우
-    dfs(n+1,sm)
-
+ 
+    dfs(n+1, sm+lst[n]) # 포함
+    dfs(n+1, sm)        # 포함X
+ 
 T = int(input())
-for test_case in range(1,T + 1):
-    # N은 개수, B는 점원들 키의 합이 이것보다는 커야
-    N, B = map(int,input().split())
+for test_case in range(1, T + 1):
+    N, B = map(int, input().split())
     lst = list(map(int, input().split()))
-
-    # 높이가 B 이상인 탑 중에서 탑의 높이와 B의 차이가 가장 작은 것 출력
+ 
     ans = N*10000
-    dfs(0,0)
+    dfs(0, 0)
     print(f'#{test_case} {ans}')
