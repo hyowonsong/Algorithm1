f0ddcfee21c8592d71c90b21382b31215b2949e9
Hyowon Song <woniwoni30@gmail.com>
2024-10-31 20:07:55 +0900

    [Silver I] Title: 미로 탐색, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,48 +1,29 @@
-# 미로탐색
+# 미로 탐색
 
-from collections import deque
+# (1,1)에서 출발하여 (N,M)의 위치로 이동할 때 지나야 하는 최소의 칸 수
 
-# N,M을 공백으로 구분하여 입력받기
-n, m = map(int, input().split())
 
-# 2차원 리스트의 맵 정보 입력받기
-graph = []
-for i in range(n):
-    graph.append(list(map(int, input())))
+def bfs(si, sj, ei, ej):
+    q = []
+    v = [[0] * M for _ in range(N)]
 
-# 이동할 네 방향 정의(상,하,좌,우)
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+    q.append((si,sj))
+    v[si][sj] = 1
 
-# BFS 소스코드 구현
-def bfs(x, y):
-    # 큐 구현을 위해 deque 라이브러리 사용
-    queue = deque([(x, y)])  # 초기 위치를 바로 큐에 추가
+    while q:
+        ci, cj = q.pop(0)
+        if (ci,cj) == (ei,ej):
+            return v[ei][ej]
+        
+        # 4방향, 범위내, 조건에 맞으면 : arr==1, v == 0
+        for di,dj in ((-1,0), (1,0), (0,-1), (0,1)):
+            ni, nj = ci+di, cj+dj
+            if 0<=ni<N and 0<=nj<M and arr[ni][nj]==1 and v[ni][nj]==0:
+                q.append((ni,nj))
+                v[ni][nj] = v[ci][cj]+1
 
-    # 큐가 빌 때까지 반복
-    while queue:
-        x, y = queue.popleft()
+N, M = map(int,input().split())
+arr = [list(map(int,input())) for _ in range(N)]
 
-        # 현재 위치에서 네 방향으로의 위치 확인
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            # 미로 찾기 공간을 벗어난 경우 무시
-            if nx < 0 or ny < 0 or nx >= n or ny >= m:
-                continue
-
-            # 벽인 경우 무시
-            if graph[nx][ny] == 0:
-                continue
-
-            # 해당 노드를 처음 방문하는 경우에만 최단 거리 기록
-            if graph[nx][ny] == 1:
-                graph[nx][ny] = graph[x][y] + 1
-                queue.append((nx, ny))
-
-    # 가장 오른쪽 아래까지의 최단 거리 반환
-    return graph[n - 1][m - 1]
-
-# BFS를 수행한 결과 출력
-print(bfs(0, 0))
+ans = bfs(0,0,N-1,M-1)
+print(ans)
