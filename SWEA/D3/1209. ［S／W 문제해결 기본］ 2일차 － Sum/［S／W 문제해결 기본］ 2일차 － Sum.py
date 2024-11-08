2956308dd8062eef4e33aeb8a49e3dc5126ccdcf
Hyowon Song <woniwoni30@gmail.com>
2024-11-08 12:18:12 +0900

    [D3] Title: [S/W 문제해결 기본] 2일차 - Sum, Time: 172 ms, Memory: 49,236 KB -BaekjoonHub

--- SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.py
+++ SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.py
@@ -1,38 +1,18 @@
-# 2일차 - Sum
-
-# 각 행의 합, 각 열의 합, 각 대각선의 합 중 최댓값을 구하는 프로그램 작성
-
 T = 10
-for t in range(1, T + 1):
-    n = int(input().strip())
-    
-    array = [list(map(int, input().strip().split())) for _ in range(100)]
-    
-    max_sum = 0  
+for test_case in range(1,T+1):
+    _ = int(input())
+    N = 100
+    arr = [list(map(int, input().split())) for _ in range(N)]
 
-    # 각 행의 합 계산
-    for i in range(100):
-        row_sum = sum(array[i])
-        max_sum = max(max_sum, row_sum)  # 최대 합 갱신
+    ans = s3 = s4 = 0
+    for i in range(N):
+        s1 = s2 = 0
+        for j in range(N):
+            s1 += arr[i][j]
+            s2 += arr[j][i]
+        ans = max(ans,s1,s2)
+        s3 += arr[i][i]
+        s4 += arr[i][N-1-i]
+    ans = max(ans, s3, s4)
 
-    # 각 열의 합 계산
-    for j in range(100):
-        col_sum = 0
-        for i in range(100):
-            col_sum += array[i][j]
-        max_sum = max(max_sum, col_sum)  # 최대 합 갱신
-
-    # 주 대각선의 합 계산
-    diag1_sum = 0
-    for i in range(100):
-        diag1_sum += array[i][i]  
-    max_sum = max(max_sum, diag1_sum)  # 최대 합 갱신
-
-    # 부 대각선의 합 계산
-    diag2_sum = 0
-    for i in range(100):
-        diag2_sum += array[i][99 - i]  # (0,99), (1,98), ..., (99,0)
-    max_sum = max(max_sum, diag2_sum)  # 최대 합 갱신
-
-    # 결과 출력
-    print(f'#{n} {max_sum}')
+    print(f'#{test_case} {ans}')
