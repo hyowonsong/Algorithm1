029b4e7379d3c0d7c752058f2943c13e97e5772e
Hyowon Song <woniwoni30@gmail.com>
2024-11-09 11:24:22 +0900

    [D2] Title: 백만 장자 프로젝트, Time: 1,571 ms, Memory: 199,396 KB -BaekjoonHub

--- SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
+++ SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
@@ -1,20 +1,21 @@
-# 총 테스트 케이스 수 입력
 T = int(input())
-
-for t in range(1, T + 1):
-    # 사재기 할 수 있는 총 일수
+for test_case in range(1, T + 1):
     N = int(input())
-    
-    # N개의 매매가 입력 받기
-    prices = list(map(int, input().split()))
-    
-    max_profit = 0
-    max_price = 0  # 현재까지 본 최대 판매 가격
-    
-    # 역순으로 매매가를 살펴봅니다.
-    for price in reversed(prices):
-        if price > max_price:
-            max_price = price  # 새로운 최대 가격 업데이트
-        max_profit += max_price - price  # 현재 가격과 최대 가격의 차이를 더함
-    
-    print(f'#{t} {max_profit}')
+    lst = list(map(int, input().split()))
+
+    ans = s = 0
+    while s < N:
+        # 현재 구간에서 가장 높은 가격을 가진 날 찾기
+        i_max = s
+        for i in range(s + 1, N):
+            if lst[i_max] < lst[i]:
+                i_max = i
+        
+        # 구간 내에서 매수하여 i_max 날에 매도, 이익 계산
+        for i in range(s, i_max):
+            ans += (lst[i_max] - lst[i])
+        
+        # i_max 이후로 이동하여 다음 구간에서 반복
+        s = i_max + 1
+
+    print(f'#{test_case} {ans}')
