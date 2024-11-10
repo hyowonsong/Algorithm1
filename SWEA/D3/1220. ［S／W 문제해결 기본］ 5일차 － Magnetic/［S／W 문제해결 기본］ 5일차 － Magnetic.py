f563f140d25607b8738cc7d3358057a17673fc31
Hyowon Song <woniwoni30@gmail.com>
2024-11-10 15:08:37 +0900

    [D3] Title: [S/W 문제해결 기본] 5일차 - Magnetic, Time: 173 ms, Memory: 50,820 KB -BaekjoonHub

---
+++ SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.py
@@ -0,0 +1,17 @@
+# magnetic
+
+T = 10
+for test_case in range(1, T + 1):
+    N = int(input())
+    arr = [list(map(int, input().split())) for _ in range(N)]
+
+    ans = 0
+    arr_t = list(zip(*arr)) # 전치행렬 만들기
+    for lst in arr_t:       # 행단위로 처리
+        prev = 0
+        for n in lst:
+            if n:
+                if n==2 and prev==1:
+                    ans += 1
+                prev = n
+    print(f'#{test_case} {ans}')
