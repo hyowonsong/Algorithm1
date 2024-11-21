ea25a08b160464b0d0b9583ea33cee4a0b9e7ea7
Hyowon Song <woniwoni30@gmail.com>
2024-11-21 12:42:42 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로1, Time: 131 ms, Memory: 45,864 KB -BaekjoonHub

--- SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.py
+++ SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.py
@@ -10,7 +10,7 @@ def bfs(si, sj):
     while q:
         ci,cj = q.pop(0)
         if arr[ci][cj]==3:
-            return 1    # 도착점에 도달했을 때 1 반환
+            return v[ci][cj]
 
         # 네방향, 범위내, 미방문, 벽이아니면 방문(q)
         for di,dj in ((-1,0),(1,0),(0,-1),(0,1)):
@@ -18,21 +18,18 @@ def bfs(si, sj):
             if 0<=ni<N and 0<=nj<N and v[ni][nj]==0 and arr[ni][nj]!=1:
                 q.append((ni,nj))
                 v[ni][nj]=1
-    return 0    # 도착점에 도달하지 못했을 때 0 반환
+    return 0
+
 
 T = 10
 for test_case in range(1, T + 1):
-    tc = input()  # 테스트 케이스 번호 입력받기
+    _ = input()
     N = 16
     arr = [list(map(int,input())) for _ in range(N)]
-    
-    # 시작점과 도착점 찾기
     for i in range(N):
         for j in range(N):
             if arr[i][j]==2:
                 si, sj = i, j
-            if arr[i][j]==3:
-                ei, ej = i, j
 
     ans = bfs(si, sj)
     print(f'#{test_case} {ans}')
