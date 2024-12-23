ca524e1a5934eb62e3cb24a6d14d9dbab2b307f9
Hyowon Song <woniwoni30@gmail.com>
2024-12-23 10:29:04 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -2,7 +2,6 @@
 # 중성화 동물은 컬럼에 Neutered, Spayed
 # 동물의 아이디, 이름, 중성화 여부를 아이디 순으로 조회
 
-
 SELECT ANIMAL_ID, NAME,  
     CASE 
         WHEN SEX_UPON_INTAKE LIKE '%Neutered%' OR SEX_UPON_INTAKE LIKE '%Spayed%' THEN 'O'
@@ -10,4 +9,3 @@ SELECT ANIMAL_ID, NAME,
     END AS 중성화 
 FROM ANIMAL_INS
 ORDER BY ANIMAL_ID;
-
