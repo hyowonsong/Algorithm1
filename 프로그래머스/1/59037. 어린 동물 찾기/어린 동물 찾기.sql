325abd947dc12eec65d1b2e5bb26dab5d463eb88
Hyowon Song <woniwoni30@gmail.com>
2024-12-28 14:50:19 +0900

    [level 1] Title: 어린 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59037. 어린 동물 찾기/어린 동물 찾기.sql
+++ 프로그래머스/1/59037. 어린 동물 찾기/어린 동물 찾기.sql
@@ -1,6 +1,6 @@
--- 코드를 입력하세요
+# 1. 동물 보호소에 들어온 동물 중 젊은 동물의 아이디 + 이름을 조회
+# 2. 결과는 아이디 순
 
-SELECT ANIMAL_ID, NAME
-FROM ANIMAL_INS
+SELECT ANIMAL_ID, NAME FROM ANIMAL_INS
 WHERE INTAKE_CONDITION != 'Aged'
-ORDER BY ANIMAL_ID 
+ORDER BY ANIMAL_ID
