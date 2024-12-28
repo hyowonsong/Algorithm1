4cc5e18a00c977a7cf3299d7bbaeb7c122bd6e0a
Hyowon Song <woniwoni30@gmail.com>
2024-12-28 19:45:44 +0900

    [level 2] Title: DATETIME에서 DATE로 형 변환, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59414. DATETIME에서 DATE로 형 변환/DATETIME에서 DATE로 형 변환.sql
+++ 프로그래머스/2/59414. DATETIME에서 DATE로 형 변환/DATETIME에서 DATE로 형 변환.sql
@@ -2,5 +2,8 @@
 # 1. 각 동물의 아이디와 이름, 들어온 날짜 조회
 # 2. 결과는 아이디 순
 
-SELECT ANIMAL_ID, NAME, DATE_FORMAT(DATETIME, '%Y-%m-%d') AS 날짜 FROM ANIMAL_INS
+# DATEFORMAT 잘 생각!
+
+SELECT ANIMAL_ID, NAME, DATE_FORMAT(DATETIME, '%Y-%m-%d') AS 날짜 
+FROM ANIMAL_INS
 ORDER BY ANIMAL_ID
