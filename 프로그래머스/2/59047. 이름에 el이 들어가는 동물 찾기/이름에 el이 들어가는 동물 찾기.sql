d318a5267d82a2b1a6b1d4f672ce210178c1f192
Hyowon Song <woniwoni30@gmail.com>
2024-12-28 15:34:42 +0900

    [level 2] Title: 이름에 el이 들어가는 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59047. 이름에 el이 들어가는 동물 찾기/이름에 el이 들어가는 동물 찾기.sql
+++ 프로그래머스/2/59047. 이름에 el이 들어가는 동물 찾기/이름에 el이 들어가는 동물 찾기.sql
@@ -1,4 +1,8 @@
+# 1.할머니가 기르던 개는 이름에 'el'이 들어간다.
+# 2.이름에 "EL"이 들어가는 개의 아이디와 이름을 조회하는 SQL문을 작성
+# 3. 결과는 이름 순으로 조회
+
 SELECT ANIMAL_ID, NAME FROM ANIMAL_INS
-WHERE LOWER(NAME) LIKE '%el%'
+WHERE NAME LIKE '%EL%'
 AND ANIMAL_TYPE = 'Dog'
 ORDER BY NAME ASC
