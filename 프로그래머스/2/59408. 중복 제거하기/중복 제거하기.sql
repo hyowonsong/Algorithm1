f131a53c07b8560405b34b1ffe828487d8797c92
Hyowon Song <woniwoni30@gmail.com>
2024-12-28 13:30:12 +0900

    [level 2] Title: 중복 제거하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59408. 중복 제거하기/중복 제거하기.sql
+++ 프로그래머스/2/59408. 중복 제거하기/중복 제거하기.sql
@@ -1,2 +1,5 @@
-SELECT COUNT(DISTINCT NAME) FROM ANIMAL_INS 
+# 동물 보호소에 들어온 동물의 이름은 몇 개인지 조회하는 SQL문을 작성
+# 이름이 NULL 인 경우는 집게하지 않으며 중복되는 이름은 하나로 칩니다.
+
+SELECT COUNT(DISTINCT NAME) FROM ANIMAL_INS
 WHERE NAME IS NOT NULL
