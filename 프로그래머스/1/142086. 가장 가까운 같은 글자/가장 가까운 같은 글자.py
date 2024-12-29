c01bd9b17b9fc6c66d4aaacde9a6fac6ae6be9fb
Hyowon Song <woniwoni30@gmail.com>
2024-12-29 19:53:16 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 1.57 ms, Memory: 10.9 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -1,12 +1,16 @@
 def solution(s):
-    answer =[]
-    s_dict = dict()
+    last_seen = {}  # 딕셔너리
+    answer = []     
 
-    for i in range(len(s)):      
-        if s[i] not in s_dict:
-            answer.append(-1)
+    for i, char in enumerate(s):
+        if char in last_seen:
+            # 해당 문자가 이전에 등장했다면 현재 인덱스와의 차이를 계산
+            answer.append(i - last_seen[char])
         else:
-            answer.append(i-s_dict[s[i]])
-        # 여기 다시 초기화해줘야함.
-        s_dict[s[i]] = i                        
+            # 이전에 등장하지 않았다면 -1 추가
+            answer.append(-1)
+
+        # 현재 문자의 위치를 갱신
+        last_seen[char] = i
+
     return answer
