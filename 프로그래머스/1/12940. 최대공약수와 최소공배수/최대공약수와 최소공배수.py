d107021ecf7131eff6ad1267d6faff1305aa2d9d
Hyowon Song <woniwoni30@gmail.com>
2024-12-29 16:55:26 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 48.10 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,13 +1,15 @@
 # 최대공약수와 최소공배수
 def solution(n,m):
     answer =[]
-    arr1=[]                 # 공약수를 저장할 리스트
+    # 최소 공약수를 저장할 리스트
+    arr1=[]                 
 
-    # 모든 수의 약수는 1부터 시작하기 때문에 1부터 시작
+    # 최소 공약수 구하기
     for i in range(1, min(n, m)+1):
-        if n%i == 0 and m%i ==0: # 동시에 나눠지면 공약수
+        if n%i == 0 and m%i ==0: 
             arr1.append(i)
 
+    # 최대 공약수 구하기
     for i in range(max(n, m), (n*m)+1):       
         # n,m 중 큰 값부터 n*m 곱한값 +1 까지 계속 for 문
         # 여기는 반대로 i가 n으로 나누어질 때 min_num = i 이다.
