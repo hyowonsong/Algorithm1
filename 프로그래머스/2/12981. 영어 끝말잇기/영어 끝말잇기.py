68e6baa025d22893abf44be90587c78bc9c2bef5
Hyowon Song <woniwoni30@gmail.com>
2024-12-30 15:28:07 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.08 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,21 +1,26 @@
+# 영어 끝말잇기
+
+# 1. 1번부터 번호 순서대로 단어를 말합니다.
+# 2. 마지막 사람이 단어를 말한 다음에는 다시 1번부터 시작합니다.
+# 3. 앞사람이 말한 단어의 마지막 문자로 시작하는 단어를 말해야 합니다.
+# 4. 이전에 등장했던 단어는 사용할 수 없습니다.
+# 5. 한 글자인 단어는 인정되지 않습니다.
+
+# 가장 먼저 탈락하는 사람의 번호와 그 사람이 몇 번째 차례에 탈락하는지 구하기
+
 def solution(n, words):
-    used_words = []
-    used_words.append(words[0])
-    last_words = words[0][-1]
-    number, order = 0, 0              # 여기 0, 0 이어야 된다.
-    
-    # 맨 처음에 0은 위에서 지정해줬으니 1부터
-    for i in range(1,len(words)):    
-        # words[i] 가 used_words에 없고 
-        # words[i]의 첫 글자가 last_words 라면
-        if words[i] not in used_words and words[i][0] == last_words:
-            used_words.append(words[i])
-            last_words = words[i][-1]
-        else:
-            # 현재 차례에 단어를 말한 사람의 번호
-            number = (i%n) + 1
-            # 현재 차례의 순서를 나타냅니다.
-            order = (i//n) +1
-            break
-            
-    return [number, order]
+    last_words = [] 
+
+    for idx, word in enumerate(words):
+        if idx > 0:  # 첫 번째 단어는 검사를 생략
+            # 앞 단어의 끝 글자와 현재 단어의 첫 글자가 다르거나, 이미 말한 단어를 다시 말한 경우
+            if words[idx - 1][-1] != word[0] or word in last_words:
+                # 탈락자 번호와 차례 계산
+                person = (idx % n) + 1
+                turn = (idx // n) + 1
+                return [person, turn]
+
+        last_words.append(word)  # 단어를 기록
+
+    return [0, 0]  # 탈락자가 없으면 [0, 0] 반환
+
