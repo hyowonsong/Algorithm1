8d428ce92a709699011b503e70f31509feb15d89
Hyowon Song <woniwoni30@gmail.com>
2024-12-30 17:49:24 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -1,11 +1,19 @@
-# 카드 뭉치
+from collections import deque
 
 def solution(cards1, cards2, goal):
-    for i in goal:
-        if cards1 and i == cards1[0]:
-            cards1.pop(0)
-        elif cards2 and i == cards2[0]:
-            cards2.pop(0)
+    cards1 = deque(cards1)
+    cards2 = deque(cards2)
+
+    for word in goal:
+        # cards1의 맨 앞 단어와 일치하면 제거
+        if cards1 and cards1[0] == word:
+            cards1.popleft()
+        # cards2의 맨 앞 단어와 일치하면 제거
+        elif cards2 and cards2[0] == word:
+            cards2.popleft()
+        # 두 카드 뭉치에 해당 단어가 없으면 "No" 반환
         else:
             return "No"
+    
+    # 모든 단어를 올바르게 사용했다면 "Yes" 반환
     return "Yes"
