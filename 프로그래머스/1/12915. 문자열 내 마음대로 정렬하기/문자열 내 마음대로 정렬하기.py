ef73510e2d4bf87113fa2b417cfda4067df1b047
Hyowon Song <woniwoni30@gmail.com>
2024-12-30 13:40:48 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.03 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.py
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.py
@@ -1,20 +1,5 @@
-# 문자열 내 마음대로 정렬하기
-# 각 문자열 n 번째 인덱스를 기준으로 오름차순 정렬
+# 1. 각 문자열의 n 번째 글자를 기준으로 오름차순 정렬
 
 def solution(strings, n):
-    answer = []
-    new_answer = []
-    strings.sort()    # 문자열 정리
-
-    for string in strings:
-        new_answer.append(string[n])     # 문자열의 n번째 단어만 append
-        new_answer.sort()           # new_answer 정리
-    for i in new_answer:
-        for string in strings:        
-            # string 돌면서 n번째 문자열이 i 이고 answer에 없다면 
-            if string[n] == i and string not in answer:      
-                answer.append(string)
-                
-            else:
-                continue
-    return answer
+    # n번째 문자를 기준으로 정렬, n번째 문자가 같으면 사전순으로 자동 정렬
+    return sorted(strings, key=lambda string: (string[n], string))
