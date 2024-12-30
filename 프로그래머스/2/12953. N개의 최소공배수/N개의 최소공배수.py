01de1ed8d8099b6cbca14f47e3c42b55b93235d5
Hyowon Song <woniwoni30@gmail.com>
2024-12-30 13:57:50 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -1,6 +1,7 @@
 # 최대공약수 = a mod b 를 반복적으로 계산하여 b=0이 되는 순간 a를 반환
 
 def gcd(a, b):
+    # b가 0이 되기전까지 while문은 돌아간다. 
     while b:
         a, b = b, a % b
     return a
@@ -9,7 +10,9 @@ def lcm(a, b):
     return (a * b) // gcd(a, b)
 
 def solution(arr):
-    result = arr[0]
+    # 맨 처음 arr[0]으로 하나 정하고 다음 숫자랑 계속 최소공배수 구하기
+    answer = arr[0]
     for num in arr[1:]:
-        result = lcm(result, num)
-    return result
+        answer = lcm(answer, num)
+    
+    return answer
