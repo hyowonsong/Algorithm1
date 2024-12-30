6a620b8542b323c7c2f04aba6189246ed3ab7752
Hyowon Song <woniwoni30@gmail.com>
2024-12-30 18:01:08 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 5324.83 ms, Memory: 43.7 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,9 +1,9 @@
 def solution(elements):
-    answer = 0  
-    cycle = elements + elements 
-    s = set() 
+    cycle = elements + elements
+    s = set()
 
-    for i in range(len(elements)):  # 리스트의 각 원소에 대해 반복
-        for j in range(1, len(elements) + 1):  # 연속된 부분 수열의 길이에 대해 반복
-            s.add(sum(cycle[i:i+j])) 
-    return len(s)  
+    for i in range(len(elements)):
+        # j는 연속 부분 수열의 길이를 의미 (1부터 elements 길이까지)
+        for j in range(1,len(elements)+1):
+            s.add(sum(cycle[i:i+j]))
+    return len(s)
