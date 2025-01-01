a2094169e7b237679dcf7dd533f62e5ebfee56b7
Hyowon Song <woniwoni30@gmail.com>
2025-01-01 19:50:24 +0900

    [level 3] Title: 정수 삼각형, Time: 39.87 ms, Memory: 14.4 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/README.md
+++ 프로그래머스/3/43105. 정수 삼각형/README.md
@@ -1,10 +1,10 @@
 # [level 3] 정수 삼각형 - 43105 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43105?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43105?language=python3) 
 
 ### 성능 요약
 
-메모리: 59.8 MB, 시간: 10.60 ms
+메모리: 14.4 MB, 시간: 39.87 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 21일 13:56:03
+2025년 01월 01일 19:50:21
 
 ### 문제 설명
 
--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -1,16 +1,10 @@
 def solution(triangle):
-    # 삼각형의 높이
-    height = len(triangle)
-    
-    # Bottom-up 방식으로 DP 배열을 초기화
-    dp = triangle[-1]  # 맨 아래층의 값들을 초기값으로 설정
-    
-    # 맨 아래층부터 위층으로 올라가면서 최대 경로 합을 계산
-    for i in range(height - 2, -1, -1):
-        for j in range(i + 1):
-            # 현재 위치 (i, j)에서의 최대 경로 합은
-            # 아래층의 (i+1, j)와 (i+1, j+1) 중 큰 값과 현재 위치의 값 triangle[i][j]를 더한 값
-            dp[j] = triangle[i][j] + max(dp[j], dp[j + 1])
-    
-    # dp[0]에는 최종적으로 꼭대기에서 바닥까지의 최대 경로 합이 저장됨
-    return dp[0]
+    # 삼각형을 아래에서부터 위로 올라가며 합을 계산
+    for row in range(len(triangle) - 2, -1, -1):
+        for col in range(len(triangle[row])):
+            
+            # 현재 위치의 값에 아래 행의 두 대각선 값 중 더 큰 값을 더함
+            triangle[row][col] += max(triangle[row + 1][col], triangle[row + 1][col + 1])
+            
+    # 최종적으로 꼭대기 값에 최대 합이 저장됨
+    return triangle[0][0]
