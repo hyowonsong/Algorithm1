91b32f4fa23687caaf367a66e7898c3929b1fdd7
Hyowon Song <woniwoni30@gmail.com>
2025-01-01 15:00:04 +0900

    [level 2] Title: 튜플, Time: 167.92 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/README.md
+++ 프로그래머스/2/64065. 튜플/README.md
@@ -1,10 +1,10 @@
 # [level 2] 튜플 - 64065 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64065?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64065) 
 
 ### 성능 요약
 
-메모리: 109 MB, 시간: 76.79 ms
+메모리: 16.4 MB, 시간: 167.92 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 20일 17:34:21
+2025년 01월 01일 15:00:02
 
 ### 문제 설명
 
--- 프로그래머스/2/64065. 튜플/튜플.py
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -1,17 +1,26 @@
 def solution(s):
-    # 문자열에서 집합을 추출
-    sets = s[2:-2].split('},{')
+    # 문자열에서 양 끝의 '{{'와 '}}' 제거, 문자열을 '},{'로 분리
+    s = s[2:-2].split('},{')
+
+    # 각 집합을 문자열에서 정수 리스트로 변환
+    lst = []
+    for group in s:
+        # 쉼표(,)를 기준으로 나눕니다.
+        numbers = group.split(',')
+        # 쉼표를 기준으로 나눈 것을 다시 리스트로 변환합니다. 
+        lst.append(list(map(int, numbers)))
+
     
-    # 각 집합을 리스트로 변환하고, 길이 순으로 정렬
-    sets = [set(map(int, x.split(','))) for x in sets]
-    sets.sort(key=len)
+    # 집합을 길이에 따라 정렬
+    lst.sort(key = len)
     
+    # 결과 튜플을 담을 리스트
     answer = []
-    for subset in sets:
-        # 집합에서 아직 추가되지 않은 원소를 찾아서 추가
-        for num in subset:
+    for group in lst:
+        for num in group:
             if num not in answer:
                 answer.append(num)
                 break
-    
+
     return answer
+    
