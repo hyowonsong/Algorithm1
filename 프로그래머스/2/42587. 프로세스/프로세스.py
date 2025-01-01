fed2fbe4a245b153cdee56f29fd928be6c8ec0a8
Hyowon Song <woniwoni30@gmail.com>
2025-01-01 16:11:01 +0900

    [level 2] Title: 프로세스, Time: 0.46 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -3,27 +3,27 @@ from collections import deque
 def solution(priorities, location):
     # 큐 초기화 (리스트를 하나씩 큐에 추가)
     queue = deque()
-    for idx, value in enumerate(priorities):
-        queue.append((value, idx))
-    
-    order = 0
-    
+    for index,prioritie in enumerate(priorities):
+        queue.append((index, prioritie))
+
+    answer = 0 
+
     while queue:
         current = queue.popleft()
-        
+
         # 큐에 있는 다른 프로세스 중 더 높은 우선순위가 있는지 확인
-        has_higher_priority = False
+        valid = False
         for i in queue:
-            if current[0] < i[0]:
-                has_higher_priority = True
+            if i[1]> current[1]:
+                valid = True
                 break
         
         # 더 높은 우선순위가 있으면 다시 큐에 삽입
-        if has_higher_priority:
-            queue.append(current)  
+        if valid :
+            queue.append(current)
         # 더 높은 우선순위가 없고 current 가 가장 크다면 프로세스를 실행
         else:
-            order += 1  
-            if current[1] == location:
-                return order  # 목표 프로세스가 실행될 순서를 반환
-                
+            answer += 1
+            if current[0] == location:
+                # 목표 프로세스가 실행될 순서를 반환
+                return answer
