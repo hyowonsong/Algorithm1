44dc4674b476a4a5d415ac98825d81f79b9b4834
Hyowon Song <woniwoni30@gmail.com>
2025-01-01 16:57:12 +0900

    [level 1] Title: 소수 만들기, Time: 20.35 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
@@ -1,21 +1,25 @@
-# 소수 만들기(아리스토테네스의 체 사용)
+# 소수 만들기
+
 # 주어진 숫자 중 3개의 수를 더했을 때 소수가 되는 경우의 개수
 
 def is_prime(num):
     if num<2:
         return False
-    else:
-        # num의 제곱근까지의 모든 수로 나누어 
-        # 나머지가 0인 경우가 있다면 소수가 아니므로 False를 반환합니다.
-        for i in range(2,int(num**0.5)+1):
-            if num % i == 0:
-                return False
-        return True
+    for i in range(2, int(num**0.5)+1):
+        if num%i == 0:
+            return False
+    return True
 
 def solution(nums):
-    from itertools import combinations
-    count = 0
-    for comb in combinations(nums, 3):    # combinations를 3개씩
-        if is_prime(sum(comb)):   # 만약 합친 것이 소수라면
-            count += 1
-    return count
+    answer = 0
+
+    # 3중 반복문으로 숫자 세 개를 선택
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            for k in range(j+1, len(nums)):
+                if is_prime(nums[i]+nums[j]+nums[k]):
+                    answer += 1
+
+    return answer
+
+print(solution([1,2,3,4]))
