a17c04ba78b3a2492e344221160bba8d571955ca
Hyowon Song <woniwoni30@gmail.com>
2025-01-02 16:01:35 +0900

    [level 1] Title: 실패율, Time: 25.18 ms, Memory: 14.9 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/README.md
+++ 프로그래머스/1/42889. 실패율/README.md
@@ -1,10 +1,10 @@
 # [level 1] 실패율 - 42889 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42889?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42889) 
 
 ### 성능 요약
 
-메모리: 87.8 MB, 시간: 28.71 ms
+메모리: 14.9 MB, 시간: 25.18 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 24일 16:12:13
+2025년 01월 02일 16:01:32
 
 ### 문제 설명
 
--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,33 +1,32 @@
+# 실패율
+
 def solution(N, stages):
-    # 각 스테이지별 실패율을 저장할 리스트
+    # 각 스테이지에 도달한 플레이어 수를 저장할 리스트
+    stage_counts = [0] * (N + 2)
+    
+    # 스테이지 별로 사용자 수 계산
+    for stage in stages:
+        stage_counts[stage] += 1
+
+    # 총 사용자 수
+    total_players = len(stages)
+    
+    # 실패율 계산
     failure_rates = []
-    
-    # 전체 사용자 수
-    total_users = len(stages)
-    
-    # 각 스테이지에 대한 실패율 계산
-    for i in range(1, N + 1):
-        # 현재 스테이지에 도달한 사용자 수
-        users_on_stage = stages.count(i)
-        
-        # 실패율 계산
-        if total_users > 0:
-            failure_rate = users_on_stage / total_users
-        else:
+    for stage in range(1, N + 1):
+        if total_players == 0:  # 해당 스테이지에 도달한 사용자가 없는 경우
             failure_rate = 0
-        
-        # 실패율과 스테이지 번호를 튜플로 저장
-        failure_rates.append((i, failure_rate))
-        
-        # 전체 사용자 수 갱신: 현재 스테이지에 도전한 사용자는 제외
-        total_users -= users_on_stage
-    
-    # 실패율을 기준으로 내림차순 정렬, 실패율이 같으면 스테이지 번호 기준 오름차순 정렬
+        else:
+            failure_rate = stage_counts[stage] / total_players
+        failure_rates.append((stage, failure_rate))
+        total_players -= stage_counts[stage]  # 다음 스테이지로 진행할 사용자 수 감소
+
+    # 실패율 기준 내림차순 정렬 (실패율이 같으면 스테이지 번호 오름차순)
+    # -를 붙이면 내림차순 정렬이 된다. 
     failure_rates.sort(key=lambda x: (-x[1], x[0]))
-    
-    # 결과에서 스테이지 번호만 추출
+
+    # 정렬된 스테이지 번호만 추출
     result = []
     for item in failure_rates:
         result.append(item[0])
-    
     return result
