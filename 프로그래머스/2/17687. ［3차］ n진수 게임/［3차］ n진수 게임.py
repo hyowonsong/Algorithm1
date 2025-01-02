a68ec5861998ce07e45c8f8e26bceecfbf3c3f11
Hyowon Song <woniwoni30@gmail.com>
2025-01-02 17:30:20 +0900

    [level 2] Title: [3차] n진수 게임, Time: 44.34 ms, Memory: 15.1 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.py
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.py
@@ -1,38 +1,46 @@
-# 숫자를 0부터 시작해서 차례대로 말한다. (0~9)
-# 10 이상 부터는 하나씩 끊어서 (1,0) (1,1) (1,2) (1,3)
-
-# 코딩 동아리 일원들은 이진수로 게임을 한다.
-
-# number를 지정된 진법 base로 변환
-def convert_to_n(number, base):
-    # 0은 모든 진법에서 '0'이므로 0인경우 바로 '0' 반환
-    if number == 0:
-        return '0'
-    # 0~15까지의 숫자를 표현하는 문자열 
-    digits = '0123456789ABCDEF'
-    
-    # 변환 결과를 저장할 빈 문자열 초기화
-    result = ''
-    while number: 
-        # 숫자를 특정 진법으로 변환
-        result = digits[number % base] + result
-        # 0 이 되는 순간 while 조건 거짓이라 끝남.
-        number //= base
-    return result
+# n진수 게임
 
 def solution(n, t, m, p):
-    # 결과 문자열, 시작 숫자, 전체 게임 시퀀스를 초기화합니다.
-    answer = ''
+    # 모든 숫자를 담을 리스트
+    sequence = []
     number = 0
-    game_sequence = ''
-    
-    # 필요한 길이(t * m)가 될 때까지 숫자를 변환하여 시퀀스에 추가
-    while len(game_sequence) < t * m:
-        game_sequence += convert_to_n(number, n)
+
+    # t * m개의 문자가 만들어질 때까지 반복
+    # 게임 참가자가 m명이고 튜브가 말해야할 것이 t개이니까 t*m
+    while len(sequence) < t * m:
+        # 현재 숫자를 n진법으로 변환
+        sequence.extend(convert_to_base(number, n))
         number += 1
+
+    # 튜브가 말해야 하는 문자만 선택
+    lst = []
+
+    # 튜브의 순서(p)에 해당하는 문자를 m 간격으로 선택
+    for i in range(p - 1, len(sequence), m):
+        lst.append(sequence[i])
+
+    # 선택된 숫자 중에서 t개만 사용
+    lst = lst[:t]
+
+    # 리스트를 문자열로 변환
+    result = "".join(lst)
+    return result 
+
+
+# n진법 변환 함수
+def convert_to_base(number, base):
+    if number == 0:
+        return "0"
     
-    # 튜브의 순서에 해당하는 숫자들만 선택하여 결과 문자열에 추가합니다
-    for i in range(p-1, t*m, m):
-        answer += game_sequence[i]
-    
-    return answer
+    digits = []
+    while number > 0:
+        remainder = number % base
+        if remainder < 10:
+            digits.append(str(remainder))
+        # 10보다 크면 변환해줘야 한다.     
+        else:
+            digits.append(chr(remainder - 10 + ord('A')))  # 10~15 -> A~F
+            
+        # number를 갱신해 다음 자리 숫자 계산.
+        number //= base
+    return digits[::-1]  # 역순으로 반환
