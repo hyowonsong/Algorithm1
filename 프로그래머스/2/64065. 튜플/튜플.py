372f4872b8fc93a945788653446c43a3dd64eb13
Hyowon Song <woniwoni30@gmail.com>
2025-01-02 11:44:58 +0900

    [level 2] Title: 튜플, Time: 237.19 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.py
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -1,13 +1,15 @@
 def solution(s):
     # 문자열에서 양 끝의 '{{'와 '}}' 제거, 문자열을 '},{'로 분리
+    # split은 항상 리스트 형태로 문자를 반환합니다. 
     s = s[2:-2].split('},{')
 
     # 각 집합을 문자열에서 정수 리스트로 변환
     lst = []
-    for group in s:
+    for i in s:
         # 쉼표(,)를 기준으로 나눕니다.
-        numbers = group.split(',')
+        numbers = i.split(',')
         # 쉼표를 기준으로 나눈 것을 다시 리스트로 변환합니다. 
+        # [[2], [2,1], [2,1,3], [2,1,3,4]]
         lst.append(list(map(int, numbers)))
 
     
@@ -16,10 +18,10 @@ def solution(s):
     
     # 결과 튜플을 담을 리스트
     answer = []
-    for group in lst:
-        for num in group:
-            if num not in answer:
-                answer.append(num)
+    for seperates in lst:
+        for seperate in seperates:
+            if seperate not in answer:
+                answer.append(seperate)
                 break
 
     return answer
