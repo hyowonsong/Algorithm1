a76612ad7251dc898f7cf69768ed879bbf382eed
Hyowon Song <woniwoni30@gmail.com>
2025-01-03 22:07:00 +0900

    [level 2] Title: 숫자 변환하기, Time: 303.46 ms, Memory: 38.7 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
@@ -1,12 +1,11 @@
-# 숫자 변환하기
-
 from collections import deque
 
 def solution(x, y, n):
+    # 방문 여부를 저장하기 위한 리스트 (y + 1까지 초기화)
+    visited = [False] * (y + 1)
     # BFS를 위한 큐 초기화
     queue = deque([(x, 0)])
-    # 방문 여부를 저장하기 위한 집합
-    visited = set([x])
+    visited[x] = True  # 시작 숫자 방문 처리
     
     while queue:
         current, operations = queue.popleft()
@@ -20,8 +19,8 @@ def solution(x, y, n):
         
         for next_state in next_states:
             # 목표 숫자를 초과하지 않고, 아직 방문하지 않은 상태인 경우 큐에 추가
-            if next_state <= y and next_state not in visited:
-                visited.add(next_state)
+            if next_state <= y and not visited[next_state]:
+                visited[next_state] = True
                 queue.append((next_state, operations + 1))
     
     # 목표 숫자에 도달할 수 없는 경우 -1 반환
