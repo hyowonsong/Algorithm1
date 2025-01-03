20391c7dad6107fdc43907972075fcfbc2cf5448
Hyowon Song <woniwoni30@gmail.com>
2025-01-03 15:11:33 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 320.72 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.py
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.py
@@ -1,16 +1,14 @@
 def solution(numbers):
-    n = len(numbers)
-    answer = [-1] * n  # 뒷 큰수 없으면 -1인 것을 미리 정의
-    
+    answer = [-1] * len(numbers)  # 뒷 큰수 없으면 -1인 것을 미리 정의
     stack = []  # 인덱스를 담을 스택
     
-    for i in range(n):
-        # stack이 비어 있지 않고 stack의 맨 마지막보다 numbers[i]가 크면
+    for i in range(len(numbers)):
+        # stack이 비어 있지 않고 stack의 맨 마지막 인덱스의 numbers보다 numbers[i]가 크면
         while stack and numbers[stack[-1]] < numbers[i]:
             idx = stack.pop()  # 현재 스택을 pop
-            answer[idx] = numbers[i]  # answer idx 초기화
+            answer[idx] = numbers[i]  # pop한 자리에 numbers[i]를 넣기
         
-        # 현재 원소의 인덱스를 스택에 추가(while 문 안돌아도 추가됨)
+        # 현재 원소의 인덱스를 스택에 추가
         stack.append(i)
     
     return answer
