81e4ab6a2d7d3d33b99edb1ea3e616313a1d48a5
Hyowon Song <woniwoni30@gmail.com>
2025-01-05 17:53:22 +0900

    [level 2] Title: 스킬트리, Time: 0.04 ms, Memory: 9.97 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.py
@@ -2,23 +2,21 @@ def solution(skill, skill_trees):
     answer = 0
 
     for skill_tree in skill_trees:
-        index = 0
-        valid = True  # 유효성을 체크하는 변수
-
+        filter = ""
+        
+        # skill에 포함된 문자만 순서대로 추출
         for s in skill_tree:
             if s in skill:
-                # 현재 skill_tree의 skill이 
-                # skill의 현재 index 위치의 스킬과 일치해야 함
-                if s == skill[index]:
-                    index += 1
-                else:
-                    # 현재 스킬이 skill의 현재 index 위치의 스킬과 다를 때
-                    # 올바른 순서가 아니므로 valid를 False로 설정
-                    valid = False
-                    break
+                filter += s
         
-        # for 루프를 다 돌고 valid가 True이면 유효한 스킬트리
-        if valid:
+        # filter가 skill의 앞부분 순서를 따르는지 확인
+        is_valid = True
+        for i in range(len(filter)):
+            if filter[i] != skill[i]:
+                is_valid = False
+                break
+        
+        if is_valid:
             answer += 1
-    
+
     return answer
