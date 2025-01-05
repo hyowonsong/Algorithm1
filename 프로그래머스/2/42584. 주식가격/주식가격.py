8e5b6fd8c5dd4e069a2222956ec011a541e94bb3
Hyowon Song <woniwoni30@gmail.com>
2025-01-05 14:10:45 +0900

    [level 2] Title: 주식가격, Time: 27.30 ms, Memory: 19.4 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,21 +1,22 @@
 def solution(prices):
-    answer = [0] * len(prices)  # 결과를 담을 리스트 초기화
-    stack = []  # 가격의 인덱스를 담을 스택
+    # 결과를 저장할 리스트를 0으로 초기화
+    answer = [0] * len(prices)
+    # 가격의 인덱스를 저장할 스택
+    stack = []
 
+    # 모든 가격을 순회
     for i in range(len(prices)):
-        # 스택이 비어있지 않고, 현재 가격이 스택의 맨 위 가격보다 작을 때
+        # 현재 가격이 스택의 마지막 가격보다 작다면, 가격이 떨어진 시점
         while stack and prices[stack[-1]] > prices[i]:
-            # 스택의 맨 위 가격 인덱스를 꺼내서
-            top = stack.pop()
-            # 현재 인덱스 i에서 스택의 맨 위 가격 인덱스 top을 빼면 가격이 떨어지지 않은 기간이 됨
-            answer[top] = i - top
+            top = stack.pop()  # 스택에서 인덱스를 꺼내고
+            answer[top] = i - top  # 현재 인덱스 top의 인덱스를 빼기
         
-        # 현재 가격의 인덱스를 스택에 추가(stack에 아무것도 없을 때 사용이 된다.)
+        # stack이 비어 있으면 추가
         stack.append(i)
     
-    # 마지막 가격은 가격이 떨어지지 않은 기간이 없으므로 0으로 설정
-    while stack:
-        top = stack.pop()
-        answer[top] = len(prices) - 1 - top
-    
+    # 루프가 끝난 이후 남아 있는 스택 처리
+    for top in stack:
+         # 끝까지 유지된 기간 계산 (len(prices)-1 은 한 몸이다.)
+        answer[top] = len(prices) - 1 - top 
+
     return answer
