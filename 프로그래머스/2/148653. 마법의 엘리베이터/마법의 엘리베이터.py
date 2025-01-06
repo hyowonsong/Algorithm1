e941bf26ddf518ff2dccf6c3617b8bff581633f4
Hyowon Song <woniwoni30@gmail.com>
2025-01-06 22:03:29 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.py
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.py
@@ -1,11 +1,14 @@
 def solution(storey):
-    answer = 0  # 필요한 마법의 돌 개수를 저장할 변수
+    answer = 0  
     
-    while storey:  # storey가 0이 될 때까지 반복
-        digit = storey % 10  # 현재 처리할 자릿수 (1의 자리부터 시작)
-        next_digit = (storey // 10) % 10  # 다음으로 높은 자릿수
+    # storey가 0이 될 때까지 반복
+    while storey:  
+        # 현재 처리할 자릿수 (1의 자리부터 시작)
+        digit = storey % 10  
+        # 다음으로 높은 자릿수
+        next_digit = (storey // 10) % 10  
         
-        # 현재 자릿수가 5보다 크거나, 5이고 다음 자릿수가 5 이상인 경우
+        # 현재 자릿수가 5보다 크거나, 5이고 다음 자릿수가 5 이상인 경우도 올림하는게 좋음
         if digit > 5 or (digit == 5 and next_digit >= 5):
             answer += (10 - digit)  # 위로 올림 (10에서 현재 자릿수를 뺀 값을 더함)
             storey += 10  # 다음 자릿수에 1을 올림
