60b1b75ddc1bcd79a7f3ae1679ab140c3e29e103
Hyowon Song <woniwoni30@gmail.com>
2025-01-06 15:12:58 +0900

    [level 3] Title: 베스트앨범, Time: 0.07 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/README.md
+++ 프로그래머스/3/42579. 베스트앨범/README.md
@@ -1,10 +1,10 @@
 # [level 3] 베스트앨범 - 42579 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579?language=python3) 
 
 ### 성능 요약
 
-메모리: 77.1 MB, 시간: 7.37 ms
+메모리: 10.2 MB, 시간: 0.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 10일 11:47:45
+2025년 01월 06일 15:12:54
 
 ### 문제 설명
 
---
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -0,0 +1,32 @@
+def solution(genres, plays):
+    # 1. 장르별 총 재생 횟수 계산
+    genre_play_count = {}
+    for genre, play in zip(genres, plays):
+        if genre in genre_play_count:
+            genre_play_count[genre] += play
+        else:
+            genre_play_count[genre] = play
+    
+    # 2. 장르별 노래 (고유 번호, 재생 횟수) 리스트 생성
+    genre_songs = {}
+    for idx, (genre, play) in enumerate(zip(genres, plays)):
+        if genre in genre_songs:
+            genre_songs[genre].append((idx, play))
+        else:
+            genre_songs[genre] = [(idx, play)]
+    
+    # 3. 각 장르별 노래 정렬 (재생 횟수 내림차순, 같은 재생 횟수에서는 고유 번호 오름차순)
+    for genre in genre_songs:
+        genre_songs[genre].sort(key=lambda x: (-x[1], x[0]))
+    
+    # 4. 장르별 총 재생 횟수 내림차순으로 장르 정렬
+    sorted_genres = sorted(genre_play_count.items(), key=lambda x: -x[1])
+    
+    result = []
+    
+    # 5. 장르별로 가장 많이 재생된 두 곡을 선택
+    for genre, _ in sorted_genres:
+        top_songs = genre_songs[genre][:2]  # 상위 2개 곡
+        result.extend([song[0] for song in top_songs])  # 고유 번호만 결과에 추가
+    
+    return result
