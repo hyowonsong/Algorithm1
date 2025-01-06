4ffdbc716a51b722c04ebd6b2d50b5be80aa2f96
Hyowon Song <woniwoni30@gmail.com>
2025-01-06 12:16:40 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 2.76 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -1,28 +1,27 @@
 def solution(files):
     # 파일명을 HEAD와 NUMBER로 파싱하는 함수
     def parse_file(file):
-        head = ''   # HEAD 부분을 저장할 변수
-        number = '' # NUMBER 부분을 저장할 변수
+        head = ""
+        number = ""
         i = 0
         length = len(file)
         
-        # HEAD 추출: 숫자가 나오기 전까지 문자를 계속 추가
+        # HEAD 추출: 첫 번째 숫자가 나올 때까지 문자 추가
         while i < length and not file[i].isdigit():
             head += file[i]
             i += 1
         
-        # NUMBER 추출: 숫자가 나오는 부분부터 최대 5자리까지 추출
+        # NUMBER 추출: 숫자가 나오는 부분부터 최대 5자리까지
         while i < length and file[i].isdigit():
             number += file[i]
             i += 1
         
-        # TAIL은 정렬에 필요 없으므로 추출하지 않음
-        return (head, number)
+        return head, number
     
-    # 정렬을 위한 키 함수
+    # 정렬 기준 함수
     def sort_key(file):
         head, number = parse_file(file)
-        return (head.lower(), int(number))
+        return (head.lower(), int(number))  # HEAD: 소문자로 비교, NUMBER: 정수 비교
     
-    # 파일명 리스트를 정렬 기준에 맞게 정렬하여 반환
+    # 파일명을 정렬 기준에 따라 정렬
     return sorted(files, key=sort_key)
