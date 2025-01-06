c20f0e386ab1d354e6210db9b8184c987be3d0de
Hyowon Song <woniwoni30@gmail.com>
2025-01-06 15:33:10 +0900

    [level 3] Title: 베스트앨범, Time: 0.10 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,11 +1,12 @@
+
 def solution(genres, plays):
     # 1. 장르별 총 재생 횟수 계산
-    genre_play_count = {}
+    count = {}
     for genre, play in zip(genres, plays):
-        if genre in genre_play_count:
-            genre_play_count[genre] += play
+        if genre in count:
+            count[genre] += play
         else:
-            genre_play_count[genre] = play
+            count[genre] = play
     
     # 2. 장르별 노래 (고유 번호, 재생 횟수) 리스트 생성
     genre_songs = {}
@@ -20,13 +21,14 @@ def solution(genres, plays):
         genre_songs[genre].sort(key=lambda x: (-x[1], x[0]))
     
     # 4. 장르별 총 재생 횟수 내림차순으로 장르 정렬
-    sorted_genres = sorted(genre_play_count.items(), key=lambda x: -x[1])
+    sorted_genres = sorted(count.items(), key=lambda x: -x[1])
     
     result = []
     
     # 5. 장르별로 가장 많이 재생된 두 곡을 선택
-    for genre, _ in sorted_genres:
+    for genre, play in sorted_genres:
         top_songs = genre_songs[genre][:2]  # 상위 2개 곡
-        result.extend([song[0] for song in top_songs])  # 고유 번호만 결과에 추가
+        for song in top_songs:
+            result.extend([song[0]])  # 고유 번호만 결과에 추가
     
     return result
