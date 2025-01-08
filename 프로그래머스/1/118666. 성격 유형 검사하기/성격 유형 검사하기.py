9063894f648b44fe2652aa44bf345e3dcb0f8874
Hyowon Song <woniwoni30@gmail.com>
2025-01-08 21:08:45 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.42 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -1,39 +1,46 @@
+# 성격 유형 검사하기
+
 def solution(survey, choices):
-    # 성격 유형 별 점수를 저장할 딕셔너리
-    scores = {'R': 0, 'T': 0, 'C': 0, 'F': 0, 
-              'J': 0, 'M': 0, 'A': 0, 'N': 0}
-    
-    # 선택지 점수 매핑
-    choice_scores = {
-        1: 3,  # 매우 비동의
-        2: 2,  # 비동의
-        3: 1,  # 약간 비동의
-        4: 0,  # 모르겠음
-        5: 1,  # 약간 동의
-        6: 2,  # 동의
-        7: 3   # 매우 동의
+    # 초기화: 성격 유형 점수 저장용 딕셔너리
+    scores = {
+        'R': 0, 'T': 0,
+        'C': 0, 'F': 0,
+        'J': 0, 'M': 0,
+        'A': 0, 'N': 0
     }
     
-    # survey, choices 따로 for문 돌리기 
+    # 점수 계산
     for i in range(len(survey)):
-        s = survey[i]
-        c = choices[i]
+        agree, disagree = survey[i][1], survey[i][0]
+        choice = choices[i]
         
-        
-        if c < 4:  # 비동의 관련 성격
-            scores[s[0]] += choice_scores[c]
-        elif c > 4:  # 동의 관련 성격
-            scores[s[1]] += choice_scores[c]
+        if choice < 4:  # 비동의
+            scores[disagree] += 4 - choice
+        elif choice > 4:  # 동의
+            scores[agree] += choice - 4
 
-    # 성격 유형을 결정하기 위한 결과 문자열
-    result = ""
-    for pair in [('R', 'T'), ('C', 'F'), ('J', 'M'), ('A', 'N')]:
-        a, b = pair
-        if scores[a] > scores[b]:
-            result += a
-        elif scores[a] < scores[b]:
-            result += b
-        else:
-            result += min(a, b)
+    # 성격 유형 결정
+    # (현재 1,2,3,4번 지표가 순서대로 있어 아래와 같이 하면 자연스럽게 순서대로 들어간다.)
+    result = []
+    if scores['R'] >= scores['T']:
+        result.append('R')
+    else:
+        result.append('T')
+
+    if scores['C'] >= scores['F']:
+        result.append('C')
+    else:
+        result.append('F')
+
+    if scores['J'] >= scores['M']:
+        result.append('J')
+    else:
+        result.append('M')
+
+    if scores['A'] >= scores['N']:
+        result.append('A')
+    else:
+        result.append('N')
     
-    return result
+    # 결과 반환
+    return ''.join(result)
