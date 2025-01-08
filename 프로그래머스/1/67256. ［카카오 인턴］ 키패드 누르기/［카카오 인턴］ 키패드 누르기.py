0b588753561ec81ed371cd407ad8a9239e6670e1
Hyowon Song <woniwoni30@gmail.com>
2025-01-08 13:11:03 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.65 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
@@ -1,43 +1,58 @@
+# 키패드 누르기
 
 def solution(numbers, hand):
-    # 키패드 위치를 좌표로 표현
+    # 키패드 좌표 매핑
     keypad = {
-        1: (0, 0), 2: (0, 1), 3: (0, 2),
-        4: (1, 0), 5: (1, 1), 6: (1, 2),
-        7: (2, 0), 8: (2, 1), 9: (2, 2),
-        '*': (3, 0), 0: (3, 1), '#': (3, 2)
+        1: (0, 0), 2: (1, 0), 3: (2, 0),
+        4: (0, 1), 5: (1, 1), 6: (2, 1),
+        7: (0, 2), 8: (1, 2), 9: (2, 2),
+        '*': (0, 3), 0: (1, 3), '#': (2, 3)
     }
     
-    # 두 위치 사이의 거리를 계산하는 함수
-    def distance(pos1, pos2):
-        return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
+    # 초기 위치 설정
+    left_pos = keypad['*']
+    right_pos = keypad['#']
     
-    left = keypad['*']  # 왼손 시작 위치
-    right = keypad['#']  # 오른손 시작 위치
-    answer = ''
+    # 결과 문자열
+    result = []
     
+    # 손잡이 설정
+    if hand == "left":
+        hand_preference = "L" 
+    else:
+        hand_preference = "R"
+    
+    # 번호 순회
     for num in numbers:
-        if num in [1, 4, 7]:
-            answer += 'L'
-            left = keypad[num]
-        elif num in [3, 6, 9]:
-            answer += 'R'
-            right = keypad[num]
-        else:
-            # 2, 5, 8, 0의 경우
-            left_dist = distance(left, keypad[num])
-            right_dist = distance(right, keypad[num])
+        current = keypad[num]
+        
+        if num in [1, 4, 7]:  # 왼쪽 열
+            result.append("L")
+            left_pos = current
+
+        elif num in [3, 6, 9]:  # 오른쪽 열
+            result.append("R")
+            right_pos = current
+
+        else:  # 가운데 열
+            # 거리 계산(맨하튼 거리 사용)
+            left_distance = abs(left_pos[0] - current[0]) + abs(left_pos[1] - current[1])
+            right_distance = abs(right_pos[0] - current[0]) + abs(right_pos[1] - current[1])
             
-            # 중앙 열(2, 5, 8, 0)의 숫자를 누를 때의 로직
-            if left_dist < right_dist or (left_dist == right_dist and hand == "left"):
-                # 왼손이 더 가깝거나, 
-                # 양손 거리가 같은데 왼손잡이인 경우
-                answer += 'L'  # 결과 문자열에 'L' 추가
-                left = keypad[num]  # 왼손 위치를 현재 숫자의 위치로 업데이트
-            else:
-                # 오른손이 더 가깝거나,
-                # 양손 거리가 같은데 오른손잡이인 경우
-                answer += 'R'  # 결과 문자열에 'R' 추가
-                right = keypad[num]  # 오른손 위치를 현재 숫자의 위치로 업데이트
+            # 왼손이 더 가까움
+            if left_distance < right_distance:  
+                result.append("L")
+                left_pos = current
+            # 오른손이 더 가까움
+            elif left_distance > right_distance:  
+                result.append("R")
+                right_pos = current
+            # 거리가 같음
+            else:  
+                result.append(hand_preference)
+                if hand_preference == "L":
+                    left_pos = current
+                else:
+                    right_pos = current
     
-    return answer
+    return "".join(result)
