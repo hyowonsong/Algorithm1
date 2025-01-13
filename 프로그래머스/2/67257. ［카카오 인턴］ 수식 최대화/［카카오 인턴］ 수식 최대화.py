f84fa8d8fbbde4f4e52190f6783ddc7fd5d2d807
Hyowon Song <woniwoni30@gmail.com>
2025-01-13 17:29:47 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 0.22 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.py
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.py
@@ -1,41 +1,69 @@
-from itertools import permutations
-
-def operation(num1, num2, op):
-    if op == '+':
-        return str(int(num1) + int(num2))
-    if op == '-':
-        return str(int(num1) - int(num2))
-    if op == '*':
-        return str(int(num1) * int(num2))
+def split_expression(expression):
+    numbers = []
+    operators = []
+    temp = ""
     
-def calculate(exp,op):
-    array=[]
-    tmp=""
-    for i in exp:
-        if i.isdigit()==True:
-            tmp+=i
+    for char in expression:
+        if char.isdigit():
+            temp += char
         else:
-            array.append(tmp)
-            array.append(i)
-            tmp=""
-    array.append(tmp)
+            numbers.append(int(temp))
+            operators.append(char)
+            temp = ""
+    numbers.append(int(temp))  # 마지막 숫자 추가
     
-    for o in op:
-        stack=[]
-        while len(array)!=0:
-            tmp=array.pop(0)
-            if tmp==o:
-                stack.append(operation(stack.pop(), array.pop(0), o))
-            else:
-                stack.append(tmp)
-        array=stack
-            
-    return abs(int(array[0]))
+    return numbers, operators
+
+def calculate(numbers, operators, priority):
+    nums = numbers[:]
+    ops = operators[:]
+    
+    # 우선순위 높은 순서대로 계산
+    for op in priority:
+        i = 0
+        while i < len(ops):
+            if ops[i] == op:
+                if op == '+':
+                    nums[i] = nums[i] + nums[i+1]
+                elif op == '-':
+                    nums[i] = nums[i] - nums[i+1]
+                else:  # '*'
+                    nums[i] = nums[i] * nums[i+1]
+                
+                nums.pop(i+1)
+                ops.pop(i)
+                i -= 1
+            i += 1
+    
+    return abs(nums[0])
+
+def make_priority(used, current, all_priorities, available):
+    if len(current) == len(available):
+        all_priorities.append(current[:])
+        return
+    
+    for i in range(len(available)):
+        if not used[i]:
+            used[i] = True
+            make_priority(used, current + [available[i]], all_priorities, available)
+            used[i] = False
 
 def solution(expression):
-    op = ['+', '-', '*']
-    op = list(permutations(op, 3))
-    result=[]
-    for i in op:
-        result.append(calculate(expression, i))
-    return max(result)
+    # 수식 분리
+    numbers, operators = split_expression(expression)
+    
+    # 사용된 연산자 종류 파악
+    available = list(set(operators))
+    
+    # 가능한 모든 우선순위 조합 생성
+    all_priorities = []
+    used = [False] * len(available)
+    make_priority(used, [], all_priorities, available)
+    
+    # 각 우선순위 조합에 대해 계산
+    max_result = 0
+    for priority in all_priorities:
+        result = calculate(numbers, operators, priority)
+        max_result = max(max_result, result)
+    
+    return max_result
