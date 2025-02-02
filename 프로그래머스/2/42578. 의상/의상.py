175989f73c690c6c0bce9f3d3f0cee13dc366e50
Hyowon Song <woniwoni30@gmail.com>
2025-02-02 17:47:07 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,18 +1,20 @@
 # 의상
 
 def solution(clothes):
-    answer = 0
-    hash_map = {}
-
-    # 의상의 이름, 의상의 종류로 이루어짐
-    for clothe,type in clothes:
-        # hash_map.get(key, default)
-        # default는 키가 딕셔너리에 없을 때 반환하는 기본 값
-        hash_map[type] = hash_map.get(type,0) + 1
-
-    answer = 1
-    for type in hash_map:
-        answer *= hash_map[type] + 1
-
-    # 모든 조합을 계산하고 아무것도 입지 않는 경우는 제외해야 하므로 최종 결과에서 1 빼기
-    return answer-1
+    # 의상 종류별로 갯수를 세기 위한 딕셔너리 생성
+    clothes_count = {}
+    
+    # clothes 배열 순회하며 딕셔너리에 카운트 저장
+    for clothe, type in clothes:
+        if type in clothes_count:
+            clothes_count[type] += 1
+        else:
+            clothes_count[type] = 1
+    
+    # 각 종류별 의상 갯수 + 1 (아무것도 입지 않은 경우를 체크)
+    combinations = 1
+    for count in clothes_count.values():
+        combinations *= (count + 1)
+    
+    # 최소 하나는 입어야 하므로, 모두 선택하지 않는 경우를 뺌
+    return combinations - 1
