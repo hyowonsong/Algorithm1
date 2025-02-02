1ff88b15f77f337da7ee8712337777cf1f1b8113
Hyowon Song <woniwoni30@gmail.com>
2025-02-02 20:53:21 +0900

    [level 2] Title: 전화번호 목록, Time: 489.00 ms, Memory: 46.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,10 +1,9 @@
 # 전화번호 목록
 
-
 def solution(phone_book):
     dict = {}
 
-    # 각 전화번호의 해시 값을 계산하여 해시 맵에 저장
+    # 각 전화번호의 해시 값을 계산하여 해시 맵에 저장(여기 저장하는게 중요)
     for i in phone_book:
         dict[i] = True
 
@@ -16,8 +15,7 @@ def solution(phone_book):
             prefix += digit      
             
             # 접두어가 해시 맵에 있다면 해당 전화번호는 다른 번호의 접두어
-            # 자기 자신인 i는 빼고 체크해야 한다. 
-            # 자기 자신을 접두어로 판단하지 않기 위해서.
+            # 자기 자신을 접두어로 판단하지 않기 위해 i는 빼고 체크
             if prefix != i and prefix in dict:
                 return False
     
