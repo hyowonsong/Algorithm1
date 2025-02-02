72c470e98fc2eafa274167a85c32d747b9fe6300
Hyowon Song <woniwoni30@gmail.com>
2025-02-02 15:08:49 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,9 +1,11 @@
 def solution(n, arr1, arr2):
     answer = []
-    # arr1과 arr2의 각 요소를 비트 OR 연산으로 결합하고, 결과를 2진수로 변환
+    
+    # n 자리만큼의 행을 만들어야
     for i in range(n):
+        # arr1과 arr2 각 요소를 비트 OR 연산으로 결합, 결과를 2진수로 변환
         tmp = bin(arr1[i] | arr2[i])
-        # 2진수 문자열에서 '0b'를 제거하고 n자리로 채우기(2진수이기 때문에 아무리커도 1)
+        # 2진수 문자열에서 '0b'를 제거하고 n자리만큼 0으로 채우기(zfill)
         tmp = tmp[2:].zfill(n)
         # '1'을 '#'으로, '0'을 ' '으로 변환
         tmp = tmp.replace('1', '#').replace('0', ' ')
