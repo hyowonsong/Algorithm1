375298dfb2863885733acfb3d488749dcd38fbfa
Hyowon Song <woniwoni30@gmail.com>
2025-02-03 13:45:20 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 12.70 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,11 +1,13 @@
 from collections import deque
 
 def bfs(maps):
+    # 맵의 행과 열을 구한다.
     n, m = len(maps), len(maps[0])
     # 상하좌우
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
     
+    # BFS 탐색을 시작하기 위해 큐를 초기화
     queue = deque([(0, 0)])
     
     # queue가 빌 때까지 반복
@@ -14,6 +16,7 @@ def bfs(maps):
         x, y = queue.popleft()
         # 상하좌우 칸 확인하기
         for i in range(4):
+            # 현재 위치에서 상하좌우로 이동 가능한 위치를 계산합니다. 
             nx, ny = x + dx[i], y + dy[i]
             # 맵을 벗어나면 무시하기
             if nx < 0 or nx >= n or ny < 0 or ny >= m:
@@ -29,9 +32,11 @@ def bfs(maps):
     # 상대 팀 진영(제일 오른쪽 아래 칸)까지의 거리 반환
     return maps[n-1][m-1]
 
+# solution 함수
 def solution(maps):
+    # bfs를 호출하고 결과를 바탕으로 정답을 반환
     answer = bfs(maps)
-    # 상대 팀 진영에 도착할 수 없을 때 -1
+    #상대 팀 진영에 도착할 수 없을 때 -1(벽에 둘러쌓여 1이되어 도착하지 못함)
     if answer == 1 :
         return -1  
     else :
