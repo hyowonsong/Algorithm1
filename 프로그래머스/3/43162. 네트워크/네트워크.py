61bc2b169eed150416e6068b627d3ac50fe7cc4a
Hyowon Song <woniwoni30@gmail.com>
2025-02-03 14:22:40 +0900

    [level 3] Title: 네트워크, Time: 0.53 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,19 +1,23 @@
-def dfs(node, visited, computers):
+# 네트워크
+
+def dfs(i, visited, computers):
     # 현재 노드를 방문 처리
-    visited[node] = True
+    visited[i] = True
     # 현재 노드와 연결된 다른 노드를 탐색
-    for neighbor in range(len(computers[node])):
-        if computers[node][neighbor] == 1 and not visited[neighbor]:
+    for neighbor in range(len(computers[i])):
+        # start와 neighbor가 연결되어 있고 neighbor를 방문하지 않았더라면
+        if computers[i][neighbor] == 1 and not visited[neighbor]:
+            # 이웃인 neighbor 쪽을 방문한다. 
             dfs(neighbor, visited, computers)
 
 def solution(n, computers):
     visited = [False] * n  # 모든 컴퓨터를 방문하지 않은 상태로 초기화
-    network_count = 0      # 네트워크 개수
+    answer = 0      # 네트워크 개수
 
     # 모든 컴퓨터를 탐색
-    for node in range(n):
-        if not visited[node]:  # 방문하지 않은 컴퓨터가 있으면
-            dfs(node, visited, computers)  # DFS로 연결된 컴퓨터를 모두 방문 처리
-            network_count += 1  # 새로운 네트워크 발견 시 증가
+    for i in range(n):
+        if not visited[i]:  # 방문하지 않은 컴퓨터가 있으면
+            dfs(i, visited, computers)  # DFS로 연결된 컴퓨터를 모두 방문 처리
+            answer += 1  # 새로운 네트워크 발견 시 증가
 
-    return network_count
+    return answer
