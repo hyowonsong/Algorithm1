abeaedc814cf05fe2dcbfba5ab01f57970351409
Hyowon Song <woniwoni30@gmail.com>
2025-02-04 22:27:39 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 375.77 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.py
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.py
@@ -4,6 +4,8 @@ def solution(numbers):
     
     for i in range(len(numbers)):
         # stack이 비어 있지 않고 stack의 맨 마지막 인덱스의 numbers보다 numbers[i]가 크면
+        # if stack을 사용하면 스택의 맨 위 값 한 개만 비교하고 끝납니다. 현재는 전부 비교해야 하기 때문에
+        # while stack을 사용하면 됩니다. 
         while stack and numbers[stack[-1]] < numbers[i]:
             idx = stack.pop()  # 현재 스택을 pop
             answer[idx] = numbers[i]  # pop한 자리에 numbers[i]를 넣기
