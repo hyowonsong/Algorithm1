c45caa1677e803906b5e40b64bfe33f6e21c2aaf
Hyowon Song <woniwoni30@gmail.com>
2025-02-04 21:25:16 +0900

    [level 2] Title: [3차] n진수 게임, Time: 58.19 ms, Memory: 15.1 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.py
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.py
@@ -1,8 +1,31 @@
 # n진수 게임
 
+# n진법 변환 함수
+def convert_to_base(number, base):
+    # number가 0인 경우
+    if number == 0:
+        return "0"
+    
+    # number가 0이 아닌 경우
+    digits = []
+    while number > 0:       # 0보다 클 때까지 계속해서 진법 변환
+        remainder = number % base
+        if remainder < 10:
+            digits.append(str(remainder))
+        # 10보다 크면 변환해줘야 한다. chr는 숫자를 문자로 반환해준다.  
+        else:
+            digits.append(chr(remainder-10+ord('A'))) # 10~15 -> A~F
+            
+        # number를 갱신해 다음 자리 숫자 계산.
+        number //= base
+        
+    #진법 맨 마지막에 있는 것을 맨 처음 계산했기 때문에 반드시 역순반환!(중요)
+    return digits[::-1]  
+
 def solution(n, t, m, p):
     # 모든 숫자를 담을 리스트
     sequence = []
+    #number는 모든 숫자를 하나씩 돌아가면서 진법 변환할 수 있게 해준다.(중요!)
     number = 0
 
     # t * m개의 문자가 만들어질 때까지 반복
@@ -23,24 +46,4 @@ def solution(n, t, m, p):
     lst = lst[:t]
 
     # 리스트를 문자열로 변환
-    result = "".join(lst)
-    return result 
-
-
-# n진법 변환 함수
-def convert_to_base(number, base):
-    if number == 0:
-        return "0"
-    
-    digits = []
-    while number > 0:
-        remainder = number % base
-        if remainder < 10:
-            digits.append(str(remainder))
-        # 10보다 크면 변환해줘야 한다.     
-        else:
-            digits.append(chr(remainder - 10 + ord('A')))  # 10~15 -> A~F
-            
-        # number를 갱신해 다음 자리 숫자 계산.
-        number //= base
-    return digits[::-1]  # 역순으로 반환
+    return "".join(lst)
