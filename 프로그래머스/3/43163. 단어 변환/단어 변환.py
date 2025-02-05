836bb6e30d712eb47b8caabd6a8fc9d16377d4ec
Hyowon Song <woniwoni30@gmail.com>
2025-02-05 21:08:02 +0900

    [level 3] Title: 단어 변환, Time: 0.19 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,3 +1,7 @@
+# 단어 변환
+# 한 단계씩 변환해 나가면서 동시에 여러 경로를 탐색한다. 
+# 처음으로 target을 찾는 순간이 곧 가장 짧은 변환 과정(가장 짧은 변환 과정을 찾기 때문에 BFS)
+
 from collections import deque
 
 def solution(begin, target, words):
@@ -19,14 +23,16 @@ def solution(begin, target, words):
         words_to_remove = []  # 방문할 단어를 기록할 리스트
         
         for word in words:
-            # 현재 단어와 비교하여 한 글자만 다를 경우
             diff_count = 0
+
             for i in range(len(current_word)):
+                # 현재 단어와 비교하여 한 글자만 다를 경우
                 if current_word[i] != word[i]:
                     diff_count += 1
                 # 한 글자 초과 다르면 break
                 if diff_count > 1:
                     break
+                
             # 한 글자만 다른 경우
             if diff_count == 1:
                 queue.append((word, current_count + 1))
