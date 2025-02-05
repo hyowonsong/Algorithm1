ee9c2b5eaf6cc750a2ea5d7261e2c8dc8f210131
Hyowon Song <woniwoni30@gmail.com>
2025-02-05 20:57:11 +0900

    [level 2] Title: 주식가격, Time: 27.25 ms, Memory: 19.5 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,3 +1,5 @@
+# 이 문제도 인덱스를 다루는 것이 핵심
+
 def solution(prices):
     # 결과를 저장할 리스트를 0으로 초기화
     answer = [0] * len(prices)
@@ -9,7 +11,7 @@ def solution(prices):
         # 현재 가격이 스택의 마지막 가격보다 작다면, 가격이 떨어진 시점
         while stack and prices[stack[-1]] > prices[i]:
             top = stack.pop()  # 스택에서 인덱스를 꺼내고
-            answer[top] = i - top  # 현재 인덱스 top의 인덱스를 빼기
+            answer[top] = i - top  # 현재 인덱스에서 top의 인덱스를 빼기
         
         # stack이 비어 있으면 추가
         stack.append(i)
