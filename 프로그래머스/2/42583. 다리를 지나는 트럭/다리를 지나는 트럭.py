2a43c02874efae6df84450ff88bbd19acc8779c4
Hyowon Song <woniwoni30@gmail.com>
2025-02-08 17:33:20 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 67.17 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -4,21 +4,23 @@ def solution(bridge_length, weight, truck_weights):
     queue = deque(truck_weights)  # 대기 중인 트럭 리스트를 큐로 변환
     bridge = deque([0] * bridge_length)  #다리 위 상태(초기에는 비어있음)
     total_weight = 0  # 현재 다리 위 트럭들의 무게 합
-    time = 0  # 경과 시간
+    answer = 0  # 경과 시간
     
     # queue가 돌아가거나 total_weight이 0보다 크면
     while queue or total_weight > 0:
-        time += 1
+        answer += 1
         # 2. 다리에서 트럭이 나옴
-        exiting_truck = bridge.popleft()
-        total_weight -= exiting_truck
+        pop_truck = bridge.popleft()
+        total_weight -= pop_truck
 
         # 1. 다리에 트럭을 올릴 수 있는지 확인
         if queue and total_weight + queue[0] <= weight:
-            entering_truck = queue.popleft()
-            bridge.append(entering_truck)
-            total_weight += entering_truck
+            enter_truck = queue.popleft()
+            bridge.append(enter_truck)
+            total_weight += enter_truck
         else:
-            bridge.append(0)  # 트럭이 없으면 빈 자리 유지
+            # 트럭이 없으면 빈 자리 유지 
+            # (2번 다리에서 계속 트럭이 나오면서 pop하기 때문에 이게 필요. 안그러면 다리 길이가 계속 줄어듬)
+            bridge.append(0)  
             
-    return time
+    return answer
