ad7136019d74397f56e9e7d2b06e9c1a0161cd26
Hyowon Song <woniwoni30@gmail.com>
2025-02-11 16:05:02 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.17 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
@@ -1,26 +1,24 @@
+# 크레인 인형
 def solution(board, moves):
-    # 바구니를 초기화
     stack = []
-    # 인형의 개수를 저장할 변수
-    count = 0
-    
-    # moves 배열의 각 위치를 순회
-    for move in moves:
-        col = move - 1  # 0-based index로 변환
-        # 맨 위에서 부터 하나씩 인형을 꺼내기
-        for row in range(len(board)):
-            if board[row][col] != 0:
-                # 꺼낸 인형
-                doll = board[row][col]
-                # 해당 위치를 빈 칸으로 설정
-                board[row][col] = 0
-                # 스택에 인형 추가
-                # 마지막 인형과 같은 경우 터트리기
-                if stack and stack[-1] == doll:
-                    stack.pop()  
-                    count += 2  # 인형 두 개의 개수를 더함
+
+    answer = 0
+    # 맨 처음에는 moves를 기준으로 움직인다.(moves는 열이기 때문에 j)
+    for j in moves:
+        j = j-1 # 0-based index
+        # 행을 한개씩 탐험하면서 
+        for i in range(len(board)):
+            # 거기에 0이 아닌 인형이 있다면
+            if board[i][j] != 0:
+                # 2가지 경우로 분류(stack의 맨 마지막과 같은 경우와 같지 않은 경우)
+                pop_doll = board[i][j]
+                board[i][j] = 0 # 해당 위치를 빈칸으로 설정
+                # 맨 마지막과 같은 경우 stack에서도 빼주고 pop_doll도 0으로 변경
+                if stack and stack[-1] == pop_doll:
+                    answer += 2
+                    stack.pop()
                 else:
-                    stack.append(doll)
+                    stack.append(pop_doll)
+                # break는 인형을 꺼낸 후 더 이상 해당 열에서 진행하지 않기 위해 사용(중요!)
                 break
-    
-    return count
+    return answer
